bb0d96e34a37aa095e5607a8dd85a5e1b2088ea0
Ran Ran <ranran@google.com>
2023-10-23 22:43:25 +0000

    Update the env to the latest one to address vulnerability

--- deployment/cloud_composer_template.tf
+++ deployment/cloud_composer_template.tf
@@ -98,7 +98,7 @@ resource "google_composer_environment" "example_environment" {
 
   config {
     software_config {
-      image_version = "composer-2.4.5-airflow-2.5.3"
+      image_version = "composer-2.4.6-airflow-2.6.3"
       airflow_config_overrides = {
         core-allowed_deserialization_classes = ".*"
       }
