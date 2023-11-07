04ca660484764b10bd5b5ec3ae0672c1943f61c7
Ran Ran <ranran@google.com>
2023-11-07 00:19:30 +0000

    Add PAX stable model tests

--- configs/vm_resource.py
+++ configs/vm_resource.py
@@ -21,13 +21,22 @@ PROJECT_CLOUD_ML_AUTO_SOLUTIONS = "cloud-ml-auto-solutions"
 
 
 class Zone(enum.Enum):
-  US_CENTRAL1_A = "us-central1-a"  # reservation for v2-32 in cloud-ml-auto-solutions
-  US_CENTRAL2_B = "us-central2-b"  # reservation for v4-8 & v4-32 in cloud-ml-auto-solutions
-  US_CENTRAL1_C = "us-central1-c"  # reservation for v2-8 in cloud-ml-auto-solutions
-  US_EAST1_D = "us-east1-d"  # reservation for v3-8 & v3-32 in cloud-ml-auto-solutions
+  US_CENTRAL1_A = (  # reservation for v2-32 in cloud-ml-auto-solutions
+      "us-central1-a"
+  )
+  US_CENTRAL2_B = (  # reservation for v4-8 & v4-32 in cloud-ml-auto-solutions
+      "us-central2-b"
+  )
+  US_CENTRAL1_C = (  # reservation for v2-8 in cloud-ml-auto-solutions
+      "us-central1-c"
+  )
+  US_EAST1_D = (  # reservation for v3-8 & v3-32 in cloud-ml-auto-solutions
+      "us-east1-d"
+  )
 
 
 class RuntimeVersion(enum.Enum):
   VM_NIGHTLY = "1vm-nightly"
   VM_NIGHTLY_POD = "1vm-nightly-pod"
   TPU_UBUNTU2204_BASE = "tpu-ubuntu2204-base"
+  TPU_VM_V4_BASE = "tpu-vm-v4-base"
