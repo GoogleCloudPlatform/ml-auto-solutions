b049292f629ebc64ad21677c435d991fe2e2ecfc
Ran Ran <ranran@google.com>
2023-12-26 21:19:04 +0000

    Update pax default v4 project name

--- configs/xlml/pax/solutionsteam_pax_supported_config.py
+++ configs/xlml/pax/solutionsteam_pax_supported_config.py
@@ -94,7 +94,7 @@ def get_pax_lm_config(
     model_name: str,
     log_dir: str,
     pax_version: PaxVersion = PaxVersion.STABLE,
-    project_name: str = vm_resource.Project.TPU_PROD_ENV_AUTOMATED.value,
+    project_name: str = vm_resource.Project.CLOUD_ML_AUTO_SOLUTIONS.value,
     ckp_path: str = "",
     extraFlags: str = "",
     network: str = "default",
