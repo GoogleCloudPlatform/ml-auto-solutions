e3de8895cb895acf3ba178eede7ee1540d2b7d8e
Will Cromar <wcromar@google.com>
2024-02-20 19:20:51 +0000

    formatting
    Change-Id: Icfd3ec316e068d2d71d120685ac7857d37f43b17

--- xlml/apis/task.py
+++ xlml/apis/task.py
@@ -552,9 +552,11 @@ class GpuGkeTask(BaseTask):
             },
         },
         "spec": {
-            "activeDeadlineSeconds": int(datetime.timedelta(
-                minutes=self.task_test_config.time_out_in_min or 60
-            ).total_seconds()),
+            "activeDeadlineSeconds": int(
+                datetime.timedelta(
+                    minutes=self.task_test_config.time_out_in_min or 60
+                ).total_seconds()
+            ),
             "backoffLimit": 0,
             "completionMode": "Indexed",
             "completions": self.task_test_config.num_hosts,
