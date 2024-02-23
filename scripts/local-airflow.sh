98ce2f63875de992fbcc4ba9ffe55b408399ae0a
Will Cromar <wcromar@google.com>
2024-02-23 21:25:47 +0000

    Add script to start new local airflow instance.
    Change-Id: Ic5cc82e722e16b5d5e1b25c6afdb75bd849a7523

---
+++ scripts/local-airflow.sh
@@ -0,0 +1,19 @@
+target_dag=$1
+
+echo "Creating Airflow instance to run $target_dag..."
+
+set -xue
+
+# TODO(wcromar): enable editable installs of `xlml` instead
+export PYTHONPATH=$PWD
+export XLMLTEST_CONFIGS=$PWD/dags/jsonnet
+
+export AIRFLOW_HOME=$(mktemp -d)
+
+mkdir $AIRFLOW_HOME/dags
+ln -s $PWD/$target_dag $AIRFLOW_HOME/dags/
+
+cd $AIRFLOW_HOME
+
+export AIRFLOW__CORE__ALLOWED_DESERIALIZATION_CLASSES='.*'
+airflow standalone
