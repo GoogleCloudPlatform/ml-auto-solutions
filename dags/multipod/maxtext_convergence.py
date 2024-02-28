c233f762dcb0c71b7cf926da3d1f6efd8f1a0bf1
gobbleturk <mattdavidow@google.com>
2024-02-28 01:25:39 +0000

    Weekly schedule

--- dags/multipod/maxtext_convergence.py
+++ dags/multipod/maxtext_convergence.py
@@ -10,58 +10,54 @@ from dags import composer_env
 from dags.multipod.configs import maxtext_gke_config, common
 from dags.multipod.configs.common import SetupMode, Platform
 
-# bf16 convergence test, takes ~ 5 hours to complete on a 4v-128
+# bf16 and int8 convergence tests, each take ~5 hours to complete on a v4-128
 
-# Run once a day at 4 am UTC (8 pm PST)
-SCHEDULED_TIME = "0 4 * * *" if composer_env.is_prod_env() else None
+# Run once a week at Saturday 6 am UTC (10 pm PST)
+SCHEDULED_TIME = "0 6 * * SAT" if composer_env.is_prod_env() else None
 
 
 with models.DAG(
     dag_id="maxtext_convergence",
     schedule=SCHEDULED_TIME,
     tags=["multipod_team", "maxtext", "stable"],
-    start_date=datetime.datetime(2024, 1, 19),
+    start_date=datetime.datetime(2024, 2, 27),
     catchup=False,
 ) as dag:
-
     
-    base_output_directory = "gs://maxtext-experiments-multipod"
+    current_time = datetime.datetime.now()
+    current_date = current_time.strftime("%Y-%m-%d")
+    base_output_directory = (
+      f"{gcs_bucket.XLML_OUTPUT_DIR}/maxtext/stable/automated/{current_date}"
+    )
     dataset_path = "gs://max-datasets-rogue"
-
-    test_name = "maxtext-convergence-bf16"
-    run_command = ((f"bash end_to_end/test_convergence_1b_params.sh OUTPUT_PATH={base_output_directory} DATASET_PATH={dataset_path}"),)
-
     job_gcp_config = gcp_config.GCPConfig(
         project_name=Project.TPU_PROD_ENV_MULTIPOD.value,
         zone=Zone.US_CENTRAL2_B.value,
         dataset_name=dataset_path,
     )
 
-    job_test_config = test_config.TpuGkeTest(
-        test_config.Tpu(
-            version=TpuVersion.V4,
-            cores=128,
-        ),
-        test_name=test_name,
-        cluster_name=ClusterName.V4_128_MULTISLICE_CLUSTER.value,
-        docker_image=DockerImage.MAXTEXT_JAX_STABLE.value,
-        run_model_cmds=run_command,
-        set_up_cmds=None,
-        time_out_in_min=600,
-        task_owner=test_owner.MATT_D,
-        num_slices=1,
-    )
+    base_convergence_command = f"bash end_to_end/test_convergence_1b_params.sh OUTPUT_PATH={base_output_directory} DATASET_PATH={dataset_path}"
+    convergence_tests = {
+        "maxtext-convergence-bf16" : ((base_convergence_command),),
+        "maxtext-convergence-int8" : ((f"export M_QUANTIZATION=int8; {base_convergence_command}"))
+    }
 
-    print(f"{job_test_config=}")
-
-    t = task.TpuXpkTask(
-        task_test_config=job_test_config,
-        task_gcp_config=job_gcp_config,
-    )
-    print(f"{t=}")
-    t.run()
-
-    # task.TpuXpkTask(
-    #     task_test_config=job_test_config,
-    #     task_gcp_config=job_gcp_config,
-    # ).run()
+    for test_name, run_command in convergence_tests.items:
+        job_test_config = test_config.TpuGkeTest(
+            test_config.Tpu(
+                version=TpuVersion.V4,
+                cores=128,
+            ),
+            test_name=test_name,
+            cluster_name=ClusterName.V4_128_MULTISLICE_CLUSTER.value,
+            docker_image=DockerImage.MAXTEXT_JAX_STABLE.value,
+            run_model_cmds=run_command,
+            set_up_cmds=None,
+            time_out_in_min=600,
+            task_owner=test_owner.MATT_D,
+            num_slices=1,
+        )
+        task.TpuXpkTask(
+            task_test_config=job_test_config,
+            task_gcp_config=job_gcp_config,
+        ).run()
