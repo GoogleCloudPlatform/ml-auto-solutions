9738b71544fef203430b18b3a0067887076cb952
Will Cromar <wcromar@google.com>
2024-02-29 21:04:36 +0000

    Increase size and count of schedulers
    Change-Id: Iaff5fb25ea761b8c0122803c6c620c14bb5aad6f

--- deployment/cloud_composer_template.tf
+++ deployment/cloud_composer_template.tf
@@ -147,10 +147,10 @@ resource "google_composer_environment" "example_environment" {
 
     workloads_config {
       scheduler {
-        cpu        = 4
-        memory_gb  = 13
+        cpu        = 8
+        memory_gb  = 26
         storage_gb = 10
-        count      = 2
+        count      = 8
       }
       web_server {
         cpu        = 2
