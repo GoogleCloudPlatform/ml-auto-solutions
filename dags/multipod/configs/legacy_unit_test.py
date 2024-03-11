baa314a47b0ab13549c4ba0c9a682ab2fc79eef8
Jon Bolin <jonbolin@google.com>
2024-03-11 20:44:51 +0000

    Example migration of XL ML test

---
+++ dags/multipod/configs/legacy_unit_test.py
@@ -0,0 +1,76 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utilities to run legacy tests from the old infrastructure."""
+
+import datetime
+import os
+from xlml.apis import gcp_config, metric_config, task, test_config
+from collections.abc import Iterable
+from dags import test_owner
+from dags.multipod.configs import common
+from dags.vm_resource import TpuVersion, Project, RuntimeVersion, ClusterName
+
+
+def get_legacy_unit_test_config(
+    test_cmd: Iterable[str],
+    tpu_version: TpuVersion,
+    tpu_cores: int,
+    tpu_zone: str,
+    time_out_in_min: int,
+    test_name: str,
+    test_mode: common.SetupMode,
+    test_owner: str,
+    docker_image: str,
+    num_slices: int = 1,
+    cluster_name: str = ClusterName.V4_8_MULTISLICE_CLUSTER.value,
+    project_name: str = Project.TPU_PROD_ENV_MULTIPOD.value,
+) -> task.TpuXpkTask:
+  """
+  Run a legacy unit test script.
+  test_cmd will be executed within the `dags/multipod/legacy_tests` directory.
+  """
+  job_gcp_config = gcp_config.GCPConfig(
+      project_name=project_name,
+      zone=tpu_zone,
+      dataset_name=metric_config.DatasetOption.XLML_DATASET,
+  )
+
+  run_model_cmds = (
+      'set -xue',
+      f'git clone https://github.com/GoogleCloudPlatform/ml-auto-solutions',
+      'cd ml-auto-solutions/dags/multipod/legacy_tests',
+      'export TPU_STDERR_LOG_LEVEL=0 TPU_MIN_LOG_LEVEL=0 JAX_USE_PJRT_C_API_ON_TPU=1 TF_CPP_MIN_LOG_LEVEL=0',
+      *test_cmd,
+  )
+
+  job_test_config = test_config.TpuGkeTest(
+      test_config.Tpu(
+          version=tpu_version,
+          cores=tpu_cores,
+      ),
+      test_name=test_name,
+      run_model_cmds=run_model_cmds,
+      set_up_cmds=None,
+      time_out_in_min=time_out_in_min,
+      task_owner=test_owner,
+      num_slices=num_slices,
+      cluster_name=cluster_name,
+      docker_image=docker_image,
+  )
+
+  return task.TpuXpkTask(
+      task_test_config=job_test_config,
+      task_gcp_config=job_gcp_config,
+  )
---
+++ dags/multipod/legacy.py
@@ -0,0 +1,77 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A DAG to run tests migrated from the legacy XL ML infrastructure"""
+
+import datetime
+from airflow import models
+from dags import composer_env, gcs_bucket, test_owner
+from dags.vm_resource import TpuVersion, Zone, Project, DockerImage, ClusterName
+from dags.multipod.configs import legacy_unit_test, gke_config
+from dags.multipod.configs.common import SetupMode, Platform
+
+# Run once a day at 9 am UTC (1 am PST)
+SCHEDULED_TIME = "0 9 * * *" if composer_env.is_prod_env() else None
+
+
+for test_mode in [SetupMode.NIGHTLY, SetupMode.STABLE]:
+  with models.DAG(
+      dag_id=f"multipod_legacy_xlml_{test_mode.value}",
+      schedule=SCHEDULED_TIME,
+      tags=["multipod_team", "xlml", "legacy", test_mode.value],
+      start_date=datetime.datetime(2024, 1, 10),
+      catchup=False,
+      concurrency=2,
+  ) as dag:
+    docker_image = (
+        DockerImage.MAXTEXT_JAX_NIGHTLY
+        if test_mode == SetupMode.NIGHTLY
+        else DockerImage.MAXTEXT_JAX_STABLE
+    )
+    current_time = datetime.datetime.now()
+    current_date = current_time.strftime("%Y-%m-%d")
+    base_output_directory = f"{gcs_bucket.XLML_OUTPUT_DIR}/legacy_unit_test/{test_mode.value}/{current_date}"
+
+    # Tests that require scripts from the `jax/unit_tests` folder should follow
+    # this pattern.
+    # TODO(jonbolin): Example for legacy unit test migration - evaluate whether
+    # to remove gpt1-like tests once test migration is complete.
+    for n_slice in [1, 2]:
+      legacy_unit_test.get_legacy_unit_test_config(
+          test_cmd=("python3 gpt1-like.py",),
+          tpu_version=TpuVersion.V4,
+          tpu_cores=16,
+          tpu_zone=Zone.US_CENTRAL2_B.value,
+          time_out_in_min=60,
+          test_name=f"gpt1-like",
+          test_mode=test_mode,
+          docker_image=docker_image.value,
+          test_owner=test_owner.JON_B,
+          num_slices=n_slice,
+          cluster_name=ClusterName.V4_16_MULTISLICE_CLUSTER.value,
+      ).run()
+
+    # Tests that run MaxText end_to_end tests should follow this pattern.
+    gke_config.get_gke_config(
+        tpu_version=TpuVersion.V4,
+        tpu_cores=8,
+        tpu_zone=Zone.US_CENTRAL2_B.value,
+        time_out_in_min=60,
+        test_name="maxtext-decode",
+        run_model_cmds=(
+            "bash end_to_end/test_decode.sh 10 gs://maxtext-xlml gs://maxtext-xlml/dataset xlml-decode-v4-8-1slice-stable",
+        ),
+        docker_image=docker_image.value,
+        test_owner=test_owner.JON_B,
+    ).run()
