dfda162b09b85e1e43bdffbe980ee91bef84521e
Surbhi Jain <sjsurbhi@google.com>
2024-03-21 18:01:04 +0000

    Add MaxText stable and nightly profiling test

--- dags/multipod/maxtext_profiling.py
+++ dags/multipod/maxtext_profiling.py
@@ -18,10 +18,9 @@ A DAG to run MaxText profiling tests.
 import datetime
 from airflow import models
 from dags import composer_env, test_owner, gcs_bucket
-from dags.vm_resource import TpuVersion, Zone, DockerImage, ClusterName
+from dags.vm_resource import TpuVersion, Zone, DockerImage
 from dags.multipod.configs import gke_config
 from dags.multipod.configs.common import SetupMode
-from xlml.apis import gcp_config, metric_config, task, test_config
 
 # Run once a day at 6 am UTC (10 pm PST)
 SCHEDULED_TIME = "0 6 * * *" if composer_env.is_prod_env() else None
@@ -30,11 +29,10 @@ with models.DAG(
     dag_id="maxtext_profiling",
     schedule=SCHEDULED_TIME,
     tags=["multipod_team", "maxtext", "stable", "nightly"],
-    start_date=datetime.datetime(2024, 3, 25),
+    start_date=datetime.datetime(2024, 3, 1),
     catchup=False,
     concurrency=2,
 ) as dag:
-  current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
   base_output_directory = gcs_bucket.XLML_OUTPUT_DIR
   dataset_path = gcs_bucket.MAXTEXT_DIR
   docker_images = [
@@ -42,31 +40,29 @@ with models.DAG(
       (SetupMode.NIGHTLY, DockerImage.MAXTEXT_JAX_NIGHTLY),
   ]
 
-  profiling_command = f'''
-  echo "Start profiling tests....."
-  cd maxtext
-  RUN_NAME='profiling_tests_{current_time}'
-  python3 MaxText/train.py MaxText/configs/base.yml run_name=\$RUN_NAME base_output_directory={base_output_directory} dataset_path={dataset_path} enable_profiler=true steps=20
-  gsutil cp -R {base_output_directory}/\$RUN_NAME/tensorboard .
-  # Uninstall both stable and nightly versions of tensorboard_plugin_profile
-  pip3 uninstall -y tbp-nightly && pip3 uninstall -y tensorboard_plugin_profile
-  # profiler test with stable tensorboard_plugin_profile
-  pip3 install tensorboard_plugin_profile
-  python3 MaxText/tests/profiler_test.py
-  # profiler test with tbp-nightly
-  pip3 uninstall -y tensorboard_plugin_profile && pip3 install tbp-nightly
-  python3 MaxText/tests/profiler_test.py
-  exit 0
-  '''
+  profiling_cmds = (
+    "export RUN_NAME='profiling_tests_$(date +%Y-%m-%d-%H-%M-%S)'",
+    "python3 MaxText/train.py MaxText/configs/base.yml"
+    f" run_name=$RUN_NAME base_output_directory={base_output_directory}"
+    f" dataset_path={dataset_path} enable_profiler=true steps=20",
+    f"gsutil cp -R {base_output_directory}/$RUN_NAME/tensorboard .",
+    "pip3 uninstall -y tbp-nightly",
+    "pip3 uninstall -y tensorboard_plugin_profile",
+    "pip3 install tensorboard_plugin_profile",
+    "python3 MaxText/tests/profiler_test.py",
+    "pip3 uninstall -y tensorboard_plugin_profile",
+    "pip3 install tbp-nightly",
+    "python3 MaxText/tests/profiler_test.py"
+  )
 
   for mode, image in docker_images:
     maxtext_v4_configs_test = gke_config.get_gke_config(
-          tpu_version=TpuVersion.V4,
-          tpu_cores=8,
-          tpu_zone=Zone.US_CENTRAL2_B.value,
-          time_out_in_min=60,
-          test_name=f"maxtext-profiling-v4-{mode.value}",
-          run_model_cmds=profiling_command,
-          docker_image=image.value,
-          test_owner=test_owner.SURBHI_J,
-      ).run()
+      tpu_version=TpuVersion.V4,
+      tpu_cores=8,
+      tpu_zone=Zone.US_CENTRAL2_B.value,
+      time_out_in_min=60,
+      test_name=f"maxtext-profiling-v4-{mode.value}",
+      run_model_cmds=profiling_cmds,
+      docker_image=image.value,
+      test_owner=test_owner.SURBHI_J,
+    ).run()
