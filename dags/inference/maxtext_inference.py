325ad434fe870f6286f57f6dd5e42e0f72428030
Andy Ye <andyye333@gmail.com>
2024-05-16 19:07:28 +0000

    Add more metrics and git commit hash

--- dags/inference/maxtext_inference.py
+++ dags/inference/maxtext_inference.py
@@ -41,28 +41,36 @@ with models.DAG(
           "tpu_version_cores": [(TpuVersion.V5E, 8), (TpuVersion.V5P, 8)],
           "checkpoint": "gs://inference-benchmarks/models/llama2-7b/2024-04-25-14-01/param-only-decode-ckpt-maxtext/checkpoints/0/items",
           "maxtext_logs": "gs://inference-benchmarks/models/llama2-7b/2024-04-25-14-01/",
+          "scan_layers": "false",
+          "dataset": "openorca",
           "weight_dtype": "bfloat16",
           "tokenizer": "tokenizer.llama2",
           "per_device_batch_sizes": [1, 2, 4, 8, 11, 12],
           # (ici_fsdp_parallelism, ici_autoregressive_parallelism, ici_tensor_parallelism)
           "ici_parallelisms": [(1, -1, 1), (1, 1, -1)],
           "request_rate": 5,
+          "num_prompts": 1000,
           "max_prefill_predict_length": 1024,
           "max_target_length": 2048,
+          "max_output_length": 1024,
       },
       "llama2-13b": {
           "sleep_time": 120,
           "tpu_version_cores": [(TpuVersion.V5E, 8), (TpuVersion.V5P, 8)],
           "checkpoint": "gs://inference-benchmarks/models/llama2-13b/2024-04-25-14-01/param-only-decode-ckpt-maxtext/checkpoints/0/items",
           "maxtext_logs": "gs://inference-benchmarks/models/llama2-13b/2024-04-25-14-01/",
+          "scan_layers": "false",
+          "dataset": "openorca",
           "weight_dtype": "bfloat16",
           "tokenizer": "tokenizer.llama2",
           "per_device_batch_sizes": [1, 2, 4, 5, 6],
           # (ici_fsdp_parallelism, ici_autoregressive_parallelism, ici_tensor_parallelism)
           "ici_parallelisms": [(1, -1, 1), (1, 1, -1)],
           "request_rate": 5,
+          "num_prompts": 1000,
           "max_prefill_predict_length": 1024,
           "max_target_length": 2048,
+          "max_output_length": 1024,
       },
       "llama2-70b": {
           "sleep_time": 240,
@@ -70,27 +78,35 @@ with models.DAG(
           "per_device_batch_sizes": [12, 16, 20, 24],
           "checkpoint": "gs://inference-benchmarks/models/llama2-70b-chat/2024-05-08-23-16/param-only-decode-ckpt-maxtext/checkpoints/0/items",
           "maxtext_logs": "gs://inference-benchmarks/models/llama2-70b-chat/2024-05-08-23-16/",
+          "scan_layers": "false",
+          "dataset": "openorca",
           "weight_dtype": "bfloat16",
           "tokenizer": "tokenizer.llama2",
           # (ici_fsdp_parallelism, ici_autoregressive_parallelism, ici_tensor_parallelism)
           "ici_parallelisms": [(1, -1, 1), (1, 1, -1)],
           "request_rate": 5,
+          "num_prompts": 1000,
           "max_prefill_predict_length": 1024,
           "max_target_length": 2048,
+          "max_output_length": 1024,
       },
       "gemma-7b": {
           "sleep_time": 120,
           "tpu_version_cores": [(TpuVersion.V5E, 8), (TpuVersion.V5P, 8)],
           "checkpoint": "gs://inference-benchmarks/models/gemma-7b/2024-04-25-14-01/param-only-decode-ckpt-maxtext/checkpoints/0/items",
           "maxtext_logs": "gs://inference-benchmarks/models/gemma-7b/2024-04-25-14-01/",
+          "scan_layers": "false",
+          "dataset": "openorca",
           "weight_dtype": "bfloat16",
           "tokenizer": "tokenizer.gemma",
           "per_device_batch_sizes": [1, 2, 4, 8, 11, 12],
           # (ici_fsdp_parallelism, ici_autoregressive_parallelism, ici_tensor_parallelism)
           "ici_parallelisms": [(1, -1, 1), (1, 1, -1)],
           "request_rate": 5,
+          "num_prompts": 1000,
           "max_prefill_predict_length": 1024,
           "max_target_length": 2048,
+          "max_output_length": 1024,
       },
   }
 
@@ -105,6 +121,8 @@ with models.DAG(
           model_configs["sleep_time"] = sweep_model_configs["sleep_time"]
           model_configs["checkpoint"] = sweep_model_configs["checkpoint"]
           model_configs["maxtext_logs"] = sweep_model_configs["maxtext_logs"]
+          model_configs["scan_layers"] = sweep_model_configs["scan_layers"]
+          model_configs["dataset"] = sweep_model_configs["dataset"]
           model_configs["weight_dtype"] = sweep_model_configs["weight_dtype"]
           model_configs["tokenizer"] = sweep_model_configs["tokenizer"]
           model_configs["per_device_batch_size"] = per_device_batch_size
@@ -115,11 +133,15 @@ with models.DAG(
           model_configs["ici_autoregressive_parallelism"] = ici_ar
           model_configs["ici_tensor_parallelism"] = ici_tensor
           model_configs["request_rate"] = sweep_model_configs["request_rate"]
+          model_configs["num_prompts"] = sweep_model_configs["num_prompts"]
           model_configs["max_target_length"] = sweep_model_configs[
               "max_target_length"
           ]
           model_configs["max_prefill_predict_length"] = sweep_model_configs[
               "max_prefill_predict_length"
+          ]
+          model_configs["max_output_length"] = sweep_model_configs[
+              "max_output_length"
           ]
 
           if tpu_version == TpuVersion.V5E:
