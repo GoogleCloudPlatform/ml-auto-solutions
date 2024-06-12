3da6aa009d007b23a25438b72beb6b923d9e0546
Param Bole <parambole@google.com>
2024-06-12 17:46:24 +0000

    Fixed formatting

--- dags/imagegen_devx/configs/jax_ss_config.py
+++ dags/imagegen_devx/configs/jax_ss_config.py
@@ -20,77 +20,6 @@ from dags.vm_resource import TpuVersion, Project, ClusterName, GpuVersion, CpuVe
 from typing import Iterable
 import datetime
 
-def get_gke_maxdiffusion_jax_ss_config(
-    tpu_version: TpuVersion,
-    tpu_cores: int,
-    tpu_zone: str,
-    time_out_in_min: int,
-    test_name: str,
-    docker_image: str,
-    test_owner: str,
-    cluster_name: str = ClusterName.V4_8_MULTISLICE_CLUSTER.value,
-    project_name: str = Project.TPU_PROD_ENV_MULTIPOD.value,
-    num_slices: int = 1,
-    dataset_name: metric_config.DatasetOption = metric_config.DatasetOption.XLML_DATASET,
-    dataset_project: str = Project.CLOUD_ML_AUTO_SOLUTIONS.value,
-    composer_project: str = Project.CLOUD_ML_AUTO_SOLUTIONS.value,
-    base_output_directory: str = None,
-    metric_aggregation_strategy: metric_config.AggregationStrategy = None,
-    user_specified_job_metric_config: metric_config.MetricConfig = None,
-) -> task.XpkTask:
-  
-  current_time = datetime.datetime.now()
-  current_date = current_time.strftime("%Y-%m-%d")
-  current_datetime = current_time.strftime("%Y-%m-%d-%H-%M-%S")
-  base_output_directory = (
-      f"{gcs_bucket.BASE_OUTPUT_DIR}/maxtext/jax-ss/automated/{current_date}"
-  )
-  run_name = f"{num_slices}slice-V{tpu_version.value}_{tpu_cores}-maxtext-jax-ss-{current_datetime}"
-
-  run_model_cmds = (
-      f"python -m src.maxdiffusion.models.train src/maxdiffusion/configs/base_2_base.yml run_name={run_name} base_output_directory={base_output_directory}",
-  )
-  job_gcp_config = gcp_config.GCPConfig(
-      project_name=project_name,
-      zone=tpu_zone,
-      dataset_name=dataset_name,
-      dataset_project=dataset_project,
-      composer_project=composer_project,
-  )
-
-  job_test_config = test_config.TpuGkeTest(
-      test_config.Tpu(
-          version=tpu_version,
-          cores=tpu_cores,
-      ),
-      test_name=test_name,
-      run_model_cmds=run_model_cmds,
-      set_up_cmds=None,
-      timeout=datetime.timedelta(minutes=time_out_in_min),
-      task_owner=test_owner,
-      num_slices=num_slices,
-      cluster_name=cluster_name,
-      docker_image=docker_image,
-  )
-  job_metric_config = user_specified_job_metric_config
-  if job_metric_config is None:
-    job_metric_config = (
-        metric_config.MetricConfig(
-            tensorboard_summary=metric_config.SummaryConfig(
-                file_location=base_output_directory,
-                aggregation_strategy=metric_aggregation_strategy,
-                use_regex_file_location=True,
-            ),
-        )
-        if base_output_directory and metric_aggregation_strategy
-        else None
-    )
-
-  return task.XpkTask(
-      task_test_config=job_test_config,
-      task_gcp_config=job_gcp_config,
-      task_metric_config=job_metric_config,
-  )
 
 def get_gke_maxtext_jax_ss_config(
     tpu_version: TpuVersion,
@@ -149,4 +78,76 @@ def get_gke_maxtext_jax_ss_config(
   return task.XpkTask(
       task_test_config=job_test_config,
       task_gcp_config=job_gcp_config,
+  )
+
+
+def get_gke_maxdiffusion_jax_ss_config(
+    tpu_version: TpuVersion,
+    tpu_cores: int,
+    tpu_zone: str,
+    time_out_in_min: int,
+    test_name: str,
+    docker_image: str,
+    test_owner: str,
+    cluster_name: str = ClusterName.V4_8_MULTISLICE_CLUSTER.value,
+    project_name: str = Project.TPU_PROD_ENV_MULTIPOD.value,
+    num_slices: int = 1,
+    dataset_name: metric_config.DatasetOption = metric_config.DatasetOption.XLML_DATASET,
+    dataset_project: str = Project.CLOUD_ML_AUTO_SOLUTIONS.value,
+    composer_project: str = Project.CLOUD_ML_AUTO_SOLUTIONS.value,
+    base_output_directory: str = None,
+    metric_aggregation_strategy: metric_config.AggregationStrategy = None,
+    user_specified_job_metric_config: metric_config.MetricConfig = None,
+) -> task.XpkTask:
+  current_time = datetime.datetime.now()
+  current_date = current_time.strftime("%Y-%m-%d")
+  current_datetime = current_time.strftime("%Y-%m-%d-%H-%M-%S")
+  base_output_directory = (
+      f"{gcs_bucket.BASE_OUTPUT_DIR}/maxtext/jax-ss/automated/{current_date}"
+  )
+  run_name = f"{num_slices}slice-V{tpu_version.value}_{tpu_cores}-maxtext-jax-ss-{current_datetime}"
+
+  run_model_cmds = (
+      f"python -m src.maxdiffusion.models.train src/maxdiffusion/configs/base_2_base.yml run_name={run_name} base_output_directory={base_output_directory}",
+  )
+  job_gcp_config = gcp_config.GCPConfig(
+      project_name=project_name,
+      zone=tpu_zone,
+      dataset_name=dataset_name,
+      dataset_project=dataset_project,
+      composer_project=composer_project,
+  )
+
+  job_test_config = test_config.TpuGkeTest(
+      test_config.Tpu(
+          version=tpu_version,
+          cores=tpu_cores,
+      ),
+      test_name=test_name,
+      run_model_cmds=run_model_cmds,
+      set_up_cmds=None,
+      timeout=datetime.timedelta(minutes=time_out_in_min),
+      task_owner=test_owner,
+      num_slices=num_slices,
+      cluster_name=cluster_name,
+      docker_image=docker_image,
+  )
+  job_metric_config = user_specified_job_metric_config
+  if job_metric_config is None:
+    job_metric_config = (
+        metric_config.MetricConfig(
+            tensorboard_summary=metric_config.SummaryConfig(
+                file_location=base_output_directory,
+                aggregation_strategy=metric_aggregation_strategy,
+                use_regex_file_location=True,
+            ),
+        )
+        if base_output_directory and metric_aggregation_strategy
+        else None
+    )
+
+  return task.XpkTask(
+      task_test_config=job_test_config,
+      task_gcp_config=job_gcp_config,
+      task_metric_config=job_metric_config,
   )
