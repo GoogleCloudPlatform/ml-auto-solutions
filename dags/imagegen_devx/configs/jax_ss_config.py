ce5282a109eb20e4bab4c9b51342c14a59e30063
Param Bole <parambole@google.com>
2024-06-17 18:38:59 +0000

    Fixed formatting

--- dags/imagegen_devx/configs/jax_ss_config.py
+++ dags/imagegen_devx/configs/jax_ss_config.py
@@ -49,6 +49,7 @@ def get_current_datetime() -> str:
   now = datetime.now() 
   return now.strftime("%Y-%m-%d-%H-%M-%S") 
 
+
 def get_gke_jax_ss_config(
     tpu_version: TpuVersion,
     tpu_cores: int,
--- dags/imagegen_devx/maxdiffusion_jax_ss_e2e.py
+++ dags/imagegen_devx/maxdiffusion_jax_ss_e2e.py
@@ -42,8 +42,8 @@ with models.DAG(
   for accelerator, slices in test_configs.items():
     cores = accelerator.rsplit("-", maxsplit=1)[-1]
     for slice_num in slices:
-       run_name = f"{slice_num}slice-V{config.tpu_versions[accelerator]}_{cores}-maxdiffusion-jax-ss-{config.get_current_datetime}"
-       maxtext_jax_ss_test = config.get_gke_jax_ss_config(
+      run_name = f"{slice_num}slice-V{config.tpu_versions[accelerator]}_{cores}-maxdiffusion-jax-ss-{config.get_current_datetime}"
+      maxtext_jax_ss_test = config.get_gke_jax_ss_config(
           tpu_version=config.tpu_versions[accelerator],
           tpu_cores=cores,
           num_slices=slice_num,
