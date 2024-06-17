9290685480569ef2634576521644a2d403413cde
Param Bole <parambole@google.com>
2024-06-17 19:33:37 +0000

    Fixed formitting using pre-commit :)

--- dags/imagegen_devx/configs/jax_ss_config.py
+++ dags/imagegen_devx/configs/jax_ss_config.py
@@ -47,8 +47,8 @@ project_names = {
 
 
 def get_current_datetime() -> str:
-   now = datetime.now()
-   return now.strftime("%Y-%m-%d-%H-%M-%S")
+  now = datetime.now()
+  return now.strftime("%Y-%m-%d-%H-%M-%S")
 
 
 def get_gke_jax_ss_config(
