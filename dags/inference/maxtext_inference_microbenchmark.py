0f5583a39d5d97b7f1c80909e05463134fd0e176
Andy Ye <andyye333@gmail.com>
2024-06-18 16:54:51 +0000

    Format

--- dags/inference/maxtext_inference_microbenchmark.py
+++ dags/inference/maxtext_inference_microbenchmark.py
@@ -64,9 +64,10 @@ def get_concatenated_list_of_params(sweep_vm_count=1):
   prefill_cache_axis_order_str_list = []
   ar_cache_axis_order_str_list = []
   for two_axis_order_product_id in range(len(two_cache_idx_product_idx_values)):
-    (prefill_cache_axis_order_idx, ar_cache_axis_order_idx,) = (
-        two_cache_idx_product_idx_values[int(two_axis_order_product_id)]
-    )
+    (
+        prefill_cache_axis_order_idx,
+        ar_cache_axis_order_idx,
+    ) = two_cache_idx_product_idx_values[int(two_axis_order_product_id)]
     prefill_cache_axis_order_str = cache_permu_idx_strs[
         prefill_cache_axis_order_idx
     ]
@@ -144,9 +145,9 @@ def generate_model_configs(
       "base_output_directory"
   ]
 
-  model_configs["inference_microbenchmark_prefill_lengths"] = (
-      sweep_model_configs["inference_microbenchmark_prefill_lengths"]
-  )
+  model_configs[
+      "inference_microbenchmark_prefill_lengths"
+  ] = sweep_model_configs["inference_microbenchmark_prefill_lengths"]
   model_configs["inference_microbenchmark_stages"] = sweep_model_configs[
       "inference_microbenchmark_stages"
   ]
@@ -159,12 +160,12 @@ def generate_model_configs(
   ]
   model_configs["reshape_q"] = sweep_model_configs["reshape_q"]
 
-  model_configs["two_axis_order_product_id_list"] = (
-      two_axis_order_product_id_concat_list[vm_number]
-  )
-  model_configs["prefill_cache_axis_order_list"] = (
-      prefill_cache_axis_order_concat_list[vm_number]
-  )
+  model_configs[
+      "two_axis_order_product_id_list"
+  ] = two_axis_order_product_id_concat_list[vm_number]
+  model_configs[
+      "prefill_cache_axis_order_list"
+  ] = prefill_cache_axis_order_concat_list[vm_number]
   model_configs["ar_cache_axis_order_list"] = ar_cache_axis_order_concat_list[
       vm_number
   ]
