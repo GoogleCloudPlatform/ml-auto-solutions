ab8ac06aacd92dc4499854ca050d94c18cc06e30
singhvijaya <singhvijaya@google.com>
2024-10-01 23:36:55 +0000

    Switch to working aqtp version

--- dags/inference/configs/jetstream_benchmark_serving_gce_config.py
+++ dags/inference/configs/jetstream_benchmark_serving_gce_config.py
@@ -65,6 +65,8 @@ def get_config(
       f"cd maxtext && bash setup.sh MODE={test_mode.value} && cd ..",
       "cd JetStream && pip install -e . && cd benchmarks && pip install -r requirements.in",
       "pip install torch --index-url https://download.pytorch.org/whl/cpu",
+      "pip uninstall aqtp",
+      "pip install aqtp==0.7.5",
   )
 
   additional_metadata_dict = {
