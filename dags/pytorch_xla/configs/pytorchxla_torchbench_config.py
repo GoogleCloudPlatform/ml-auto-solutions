ef23ec3c1ec8699654dedf0ecf0d98ebdcfcce12
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-10-29 17:20:19 -0700

    [PyTorch/XLA 2.5.1 release] Update common.libsonnet for v2.5.1 (#456)
    * Update common.libsonnet
    
    * Update pytorchxla_torchbench_config.py

--- dags/pytorch_xla/configs/pytorchxla_torchbench_config.py
+++ dags/pytorch_xla/configs/pytorchxla_torchbench_config.py
@@ -98,16 +98,16 @@ class VERSION_MAPPING:
     TORCH_XLA_REPO_BRANCH = "-b v2.5.0"
 
   class R2_5_1(enum.Enum):
-    TORCH_XLA_TPU_WHEEL = "https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch_xla-2.5.1rc1-cp310-cp310-manylinux_2_28_x86_64.whl"
-    TORCH_XLA_CUDA_WHEEL = "https://storage.googleapis.com/pytorch-xla-releases/wheels/cuda/12.1/torch_xla-2.5.0rc1-cp310-cp310-linux_x86_64.whl"
-    TORCH = "https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch-2.5.1rc1-cp310-cp310-linux_x86_64.whl"
-    TORCHVISION = "torchvision==0.19.0"
+    TORCH_XLA_TPU_WHEEL = "https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch_xla-2.5.1-cp310-cp310-manylinux_2_28_x86_64.whl"
+    TORCH_XLA_CUDA_WHEEL = "https://storage.googleapis.com/pytorch-xla-releases/wheels/cuda/12.1/torch_xla-2.5.0-cp310-cp310-linux_x86_64.whl"
+    TORCH = "torch==2.5.1"
+    TORCHVISION = "torchvision==0.20.0"
     TORCHAUDIO = "torchaudio==2.5.0"
     TORCH_XLA_GPU_DOCKER = "us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:r2.5.0_3.10_cuda_12.1"
     TORCH_INDEX_CPU_URL = "https://download.pytorch.org/whl/test/cpu"
     TORCH_INDEX_CUDA_URL = "https://download.pytorch.org/whl/test/cu121"
-    TORCH_REPO_BRANCH = "-b v2.5.1-rc1"
-    TORCH_XLA_REPO_BRANCH = "-b v2.5.1-rc1"
+    TORCH_REPO_BRANCH = "-b v2.5.1"
+    TORCH_XLA_REPO_BRANCH = "-b v2.5.1"
 
 
 def get_version_mapping(test_version):
