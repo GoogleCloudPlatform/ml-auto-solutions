b084ec1bc713a4f6b13b280a5a288e0d97e5c47e
Akanksha Gupta <akshu@google.com>
2024-11-05 06:08:00 +0000

    PR comments

--- dags/multipod/configs/jax_tests_gce_config.py
+++ dags/multipod/configs/jax_tests_gce_config.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utilities to construct configs for JAX tests."""
+"""Utilities to construct configs for JAX tests for GCE."""
 
 from xlml.apis import gcp_config, metric_config, task, test_config
 from dags import test_owner, gcs_bucket
