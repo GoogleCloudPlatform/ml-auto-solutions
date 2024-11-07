217fa23700708fe45d4b401796cb8e39df4be78b
Param Bole <parambole@google.com>
2024-11-07 00:34:12 +0000

    Adding AxLearn work for stable stack e2e test

--- dags/imagegen_devx/jax_stable_stack_e2e.py
+++ dags/imagegen_devx/jax_stable_stack_e2e.py
@@ -44,6 +44,10 @@ with models.DAG(
       "v4-8": [1],
       "v6e-256": [1],
   }
+  axlearn_test_configs = {
+      # accelerator: list of slices to test
+      "v4-16": [1, 2],
+  }
   train_base = (
       "python3 MaxText/train.py MaxText/configs/base.yml "
       "base_output_directory=gs://runner-maxtext-logs dataset_path=gs://maxtext-dataset "
@@ -76,6 +80,21 @@ with models.DAG(
           docker_image=DockerImage.MAXTEXT_TPU_JAX_STABLE_STACK.value,
           test_owner=test_owner.PARAM_B,
       ).run()
+      axlearn_jax_stable_stack_test = config.get_gke_config(
+          num_slices=slice_num,
+          cluster=cluster,
+          time_out_in_min=180,
+          run_model_cmds=(
+              "JAX_PLATFORMS=tpu,cpu ENABLE_PJRT_COMPATIBILITY=true TPU_SLICE_BUILDER_DUMP_CHIP_FORCE=true TPU_SLICE_BUILDER_DUMP_ICI=true JAX_FORCE_TPU_INIT=true ENABLE_TPUNETD_CLIENT=true && "
+              "cd axlearn && python -m axlearn.common.launch_trainer_main "
+              f"--module=text.gpt.c4_trainer --config=fuji-test-v1 "
+              f"--trainer_dir={gcs_bucket.BASE_OUTPUT_DIR}/bite/jax-stable-stack/automated/{current_datetime} "
+              f"--data_dir={gcs_bucket.AXLEARN_DIR} --jax_backend=tpu ",
+          ),
+          test_name=f"axlearn-jax-stable-stack-{accelerator}-{slice_num}x",
+          docker_image=DockerImage.AXLEARN_TPU_JAX_STABLE_STACK.value,
+          test_owner=test_owner.PARAM_B,
+      ).run()
 
   for accelerator, slices in maxdiffusion_test_configs.items():
     cores = accelerator.rsplit("-", maxsplit=1)[-1]
@@ -93,6 +112,26 @@ with models.DAG(
           ),
           test_name=f"maxdiffusion-jax-stable-stack-{accelerator}-{slice_num}x",
           docker_image=DockerImage.MAXDIFFUSION_TPU_JAX_STABLE_STACK.value,
+          test_owner=test_owner.PARAM_B,
+      ).run()
+
+  for accelerator, slices in axlearn_test_configs.items():
+    cores = accelerator.rsplit("-", maxsplit=1)[-1]
+    cluster = config.clusters[accelerator]
+    for slice_num in slices:
+        axlearn_jax_stable_stack_test = config.get_gke_config(
+          num_slices=slice_num,
+          cluster=cluster,
+          time_out_in_min=180,
+          run_model_cmds=(
+              "JAX_PLATFORMS=tpu,cpu ENABLE_PJRT_COMPATIBILITY=true TPU_SLICE_BUILDER_DUMP_CHIP_FORCE=true TPU_SLICE_BUILDER_DUMP_ICI=true JAX_FORCE_TPU_INIT=true ENABLE_TPUNETD_CLIENT=true && "
+              "cd axlearn && python -m axlearn.common.launch_trainer_main "
+              f"--module=text.gpt.c4_trainer --config=fuji-test-v1 "
+              f"--trainer_dir={gcs_bucket.BASE_OUTPUT_DIR}/bite/jax-stable-stack/automated/{current_datetime} "
+              f"--data_dir={gcs_bucket.AXLEARN_DIR} --jax_backend=tpu ",
+          ),
+          test_name=f"axlearn-jax-stable-stack-{accelerator}-{slice_num}x",
+          docker_image=DockerImage.AXLEARN_TPU_JAX_STABLE_STACK.value,
           test_owner=test_owner.PARAM_B,
       ).run()
 
