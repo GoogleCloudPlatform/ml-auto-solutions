9dc93597cd02e9a524ae6e6e0586c140f7059b68
Yijia <jinyijia24@gmail.com>
2024-11-14 10:27:57 -0800

    Fix Project Image Name for GPU Inference DAGs (#475)
    * fix project name
    
    * format

--- dags/vm_resource.py
+++ dags/vm_resource.py
@@ -67,6 +67,7 @@ class ImageProject(enum.Enum):
   """Common image projects for GPU."""
 
   DEEP_LEARNING_PLATFORM_RELEASE = "deeplearning-platform-release"
+  ML_IMAGES = "ml-images"
 
 
 class ImageFamily(enum.Enum):
