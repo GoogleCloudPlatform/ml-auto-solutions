488c76bce2c53f1bb944bf34aaeb145533a39348
Maggie Zhang <jiya.zhang.98@gmail.com>
2024-11-15 03:29:49 +0000

    Add AXLearn unit test on TPU

--- dags/sparsity_diffusion_devx/project_bite_tpu_e2e.py
+++ dags/sparsity_diffusion_devx/project_bite_tpu_e2e.py
@@ -55,3 +55,11 @@ with models.DAG(
       pinned_version="e918d7c219d067dfcace8a25e619d90c5a54c36b",
       time_out_in_min=180,
   )
+
+  jax_axlearn_unit_test_tpu = config.get_bite_unit_test_config(
+      tpu_version=TpuVersion.V4,
+      tpu_cores=8,
+      tpu_zone=Zone.US_CENTRAL2_B.value,
+      runtime_version=RuntimeVersion.TPU_UBUNTU2204_BASE.value,
+      time_out_in_min=180,
+  )
