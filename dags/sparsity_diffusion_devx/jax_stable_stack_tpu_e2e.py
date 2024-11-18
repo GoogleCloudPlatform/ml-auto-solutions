d30999d45a27730d105ad9088e71d0b6dd0ccd45
Param Bole <parambole@google.com>
2024-11-18 08:29:30 -0800

    Adding new tests with stable stack nightly jax images (#476)
    * Adding new tests with stable stack nightly jax images
    
    * Correcting Typo

--- dags/sparsity_diffusion_devx/jax_stable_stack_tpu_e2e.py
+++ dags/sparsity_diffusion_devx/jax_stable_stack_tpu_e2e.py
@@ -21,6 +21,7 @@ from airflow.utils.task_group import TaskGroup
 from dags import composer_env, test_owner, gcs_bucket
 from dags.vm_resource import Project, TpuVersion, CpuVersion, Zone, DockerImage, GpuVersion, XpkClusters
 from dags.sparsity_diffusion_devx.configs import gke_config as config
+from dags.multipod.configs.common import SetupMode
 from xlml.utils import name_format
 
 # Run once a day at 3 am UTC (7 pm PST)
@@ -62,45 +63,52 @@ with models.DAG(
       group_id="Quarantine", dag=dag, prefix_group_id=False
   )
 
+  docker_images = [
+      (SetupMode.STABLE, DockerImage.MAXTEXT_TPU_JAX_STABLE_STACK),
+      (SetupMode.NIGHTLY, DockerImage.MAXTEXT_TPU_STABLE_STACK_NIGHTLY_JAX),
+  ]
+
   for accelerator, slices in maxtext_test_configs.items():
     cores = accelerator.rsplit("-", maxsplit=1)[-1]
     cluster = config.clusters[accelerator]
     for slice_num in slices:
-      maxtext_jax_stable_stack_test = config.get_gke_config(
-          num_slices=slice_num,
-          cluster=cluster,
-          time_out_in_min=60,
-          run_model_cmds=(
-              f"JAX_PLATFORMS=tpu,cpu ENABLE_PJRT_COMPATIBILITY=true TPU_SLICE_BUILDER_DUMP_CHIP_FORCE=true TPU_SLICE_BUILDER_DUMP_ICI=true JAX_FORCE_TPU_INIT=true ENABLE_TPUNETD_CLIENT=true && "
-              f"python MaxText/train.py MaxText/configs/base.yml run_name={slice_num}slice-V{cluster.device_version}_{cores}-maxtext-jax-stable-stack-{current_datetime} "
-              "steps=30 per_device_batch_size=1 max_target_length=4096 model_name=llama2-7b "
-              "enable_checkpointing=false attention=dot_product remat_policy=minimal_flash use_iota_embed=true scan_layers=false "
-              "dataset_type=synthetic async_checkpointing=false "
-              f"base_output_directory={gcs_bucket.BASE_OUTPUT_DIR}/maxtext/jax-stable-stack/automated/{current_datetime}",
-          ),
-          test_name=f"maxtext-jax-stable-stack-{accelerator}-{slice_num}x",
-          docker_image=DockerImage.MAXTEXT_TPU_JAX_STABLE_STACK.value,
-          test_owner=test_owner.PARAM_B,
-      ).run_with_quarantine(quarantine_task_group)
+      for mode, image in docker_images:
+        maxtext_jax_stable_stack_test = config.get_gke_config(
+            num_slices=slice_num,
+            cluster=cluster,
+            time_out_in_min=60,
+            run_model_cmds=(
+                f"JAX_PLATFORMS=tpu,cpu ENABLE_PJRT_COMPATIBILITY=true TPU_SLICE_BUILDER_DUMP_CHIP_FORCE=true TPU_SLICE_BUILDER_DUMP_ICI=true JAX_FORCE_TPU_INIT=true ENABLE_TPUNETD_CLIENT=true && "
+                f"python MaxText/train.py MaxText/configs/base.yml run_name={slice_num}slice-V{cluster.device_version}_{cores}-maxtext-jax-stable-stack-{current_datetime} "
+                "steps=30 per_device_batch_size=1 max_target_length=4096 model_name=llama2-7b "
+                "enable_checkpointing=false attention=dot_product remat_policy=minimal_flash use_iota_embed=true scan_layers=false "
+                "dataset_type=synthetic async_checkpointing=false "
+                f"base_output_directory={gcs_bucket.BASE_OUTPUT_DIR}/maxtext/jax-stable-stack/automated/{current_datetime}",
+            ),
+            test_name=f"maxtext-jax-stable-stack-{mode.value}-{accelerator}-{slice_num}x",
+            docker_image=DockerImage.MAXTEXT_TPU_JAX_STABLE_STACK.value,
+            test_owner=test_owner.PARAM_B,
+        ).run_with_quarantine(quarantine_task_group)
 
   for accelerator, slices in maxdiffusion_test_configs.items():
     cores = accelerator.rsplit("-", maxsplit=1)[-1]
     cluster = config.clusters[accelerator]
     for slice_num in slices:
-      maxdiffusion_jax_stable_stack_test = config.get_gke_config(
-          num_slices=slice_num,
-          cluster=cluster,
-          time_out_in_min=60,
-          run_model_cmds=(
-              f"JAX_PLATFORMS=tpu,cpu ENABLE_PJRT_COMPATIBILITY=true TPU_SLICE_BUILDER_DUMP_CHIP_FORCE=true TPU_SLICE_BUILDER_DUMP_ICI=true JAX_FORCE_TPU_INIT=true ENABLE_TPUNETD_CLIENT=true && "
-              f"pip install . && python src/maxdiffusion/train.py src/maxdiffusion/configs/base_2_base.yml "
-              f"run_name={slice_num}slice-V{cluster.device_version}_{cores}-maxdiffusion-jax-stable-stack-{current_datetime} "
-              f"output_dir={gcs_bucket.BASE_OUTPUT_DIR}/maxdiffusion/jax-stable-stack/automated/{current_datetime}",
-          ),
-          test_name=f"maxdiffusion-jax-stable-stack-{accelerator}-{slice_num}x",
-          docker_image=DockerImage.MAXDIFFUSION_TPU_JAX_STABLE_STACK.value,
-          test_owner=test_owner.PARAM_B,
-      ).run_with_quarantine(quarantine_task_group)
+      for mode, image in docker_images:
+        maxdiffusion_jax_stable_stack_test = config.get_gke_config(
+            num_slices=slice_num,
+            cluster=cluster,
+            time_out_in_min=60,
+            run_model_cmds=(
+                f"JAX_PLATFORMS=tpu,cpu ENABLE_PJRT_COMPATIBILITY=true TPU_SLICE_BUILDER_DUMP_CHIP_FORCE=true TPU_SLICE_BUILDER_DUMP_ICI=true JAX_FORCE_TPU_INIT=true ENABLE_TPUNETD_CLIENT=true && "
+                f"pip install . && python src/maxdiffusion/train.py src/maxdiffusion/configs/base_2_base.yml "
+                f"run_name={slice_num}slice-V{cluster.device_version}_{cores}-maxdiffusion-jax-stable-stack-{current_datetime} "
+                f"output_dir={gcs_bucket.BASE_OUTPUT_DIR}/maxdiffusion/jax-stable-stack/automated/{current_datetime}",
+            ),
+            test_name=f"maxdiffusion-jax-stable-stack-{mode.value}-{accelerator}-{slice_num}x",
+            docker_image=DockerImage.MAXDIFFUSION_TPU_JAX_STABLE_STACK.value,
+            test_owner=test_owner.PARAM_B,
+        ).run_with_quarantine(quarantine_task_group)
 
   for accelerator, slices in axlearn_test_configs.items():
     cores = accelerator.rsplit("-", maxsplit=1)[-1]
