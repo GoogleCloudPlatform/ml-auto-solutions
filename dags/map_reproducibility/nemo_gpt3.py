3db020ef225e02db746ed691ff7c6cdb19fb5c70
Gunjan Jalori <gunjanjalori@google.com>
2024-11-21 13:50:59 -0800

    add GoB cloning commands

--- dags/map_reproducibility/nemo_gpt3.py
+++ dags/map_reproducibility/nemo_gpt3.py
@@ -21,12 +21,14 @@ from airflow.hooks.subprocess import SubprocessHook
 from dags import composer_env
 from dags.map_reproducibility.aotc_reproducibility import get_metrics_cmds
 from dags.map_reproducibility.aotc_reproducibility import set_variables_cmds
-from dags.map_reproducibility.aotc_reproducibility import set_project_commands
+from dags.map_reproducibility.aotc_reproducibility import configure_project_and_cluster
 from dags.map_reproducibility.aotc_reproducibility import install_helm_cmds
 from dags.map_reproducibility.aotc_reproducibility import namespace_cmds
 from dags.map_reproducibility.aotc_reproducibility import wait_for_jobs_cmds
 from dags.map_reproducibility.aotc_reproducibility import copy_bucket_cmds
 from dags.map_reproducibility.aotc_reproducibility import cleanup_cmds
+from dags.map_reproducibility.aotc_reproducibility import git_cookie_authdaemon
+from dags.map_reproducibility.aotc_reproducibility import clone_gob
 
 # Run once a day at 2 pm UTC (6 am PST)
 SCHEDULED_TIME = "0 14 * * *" if composer_env.is_prod_env() else None
@@ -34,50 +36,51 @@ SCHEDULED_TIME = "0 14 * * *" if composer_env.is_prod_env() else None
 
 @task
 def run_aotc_workload():
-  gpu_recipe_cmd = (
-      "git clone https://github.com/ai-hypercomputer/gpu-recipes.git",
-      "cd gpu-recipes",
-      "export REPO_ROOT=`git rev-parse --show-toplevel`",
-      "export RECIPE_ROOT="
-      "$REPO_ROOT/training/a3mega/gpt3-175b/nemo-pretraining-gke",
-      "cd $RECIPE_ROOT",
-  )
+    gpu_recipe_cmd = (
+        "export REPO_ROOT=`pwd`",
+        "export RECIPE_ROOT="
+        "$REPO_ROOT/training/a3mega/gpt3-175b/nemo-pretraining-gke",
+        "cd $RECIPE_ROOT",
+    )
 
-  helm_cmds = (
-      "CONFIG_FILE=$REPO_ROOT/src/frameworks"
-      "/nemo-configs/gpt3-175b-256gpus-fp8.yaml",
-      " helm install -f values.yaml "
-      "--namespace default "
-      "--set namespace=default"
-      " --set-file nemo_config"
-      "=$CONFIG_FILE"
-      " --set workload.image"
-      "=us-central1-docker.pkg.dev/"
-      "supercomputer-testing/gunjanjalori/nemo_test/nemo_workload:24.07"
-      " --set workload.gcsBucketForDataCataPath=$BUCKET_NAME"
-      " $JOB_NAME $REPO_ROOT/src/helm-charts/nemo-training",
-  )
+    helm_cmds = (
+        "CONFIG_FILE=$REPO_ROOT/src/frameworks"
+        "/nemo-configs/gpt3-175b-256gpus-fp8.yaml",
+        "export JOB_NAME=gpt3-xlml-$NOW-175b-nemo",
+        " helm install -f values.yaml "
+        "--namespace default "
+        "--set namespace=default"
+        " --set-file nemo_config"
+        "=$CONFIG_FILE"
+        " --set workload.image"
+        "=us-central1-docker.pkg.dev/"
+        "supercomputer-testing/gunjanjalori/nemo_test/nemo_workload:24.07"
+        " --set workload.gcsBucketForDataCataPath=$BUCKET_NAME"
+        " $JOB_NAME $REPO_ROOT/src/helm-charts/nemo-training",
+    )
 
-  hook = SubprocessHook()
-  result = hook.run_command(
-      [
-          "bash",
-          "-c",
-          ";".join(
-              set_variables_cmds()
-              + set_project_commands()
-              + gpu_recipe_cmd
-              + install_helm_cmds()
-              + namespace_cmds()
-              + helm_cmds
-              + wait_for_jobs_cmds()
-              + copy_bucket_cmds()
-              + get_metrics_cmds()
-              + cleanup_cmds()
-          ),
-      ],
-  )
-  assert result.exit_code == 0, f"Command failed with code {result.exit_code}"
+    hook = SubprocessHook()
+    result = hook.run_command(
+        [
+            "bash",
+            "-c",
+            ";".join(
+                set_variables_cmds()
+                + configure_project_and_cluster()
+                + git_cookie_authdaemon()
+                + clone_gob()
+                + gpu_recipe_cmd
+                + install_helm_cmds()
+                + namespace_cmds()
+                + helm_cmds
+                + wait_for_jobs_cmds()
+                + copy_bucket_cmds()
+                + get_metrics_cmds()
+                + cleanup_cmds()
+            ),
+        ],
+    )
+    assert result.exit_code == 0, f"Command failed with code {result.exit_code}"
 
 
 with models.DAG(
@@ -94,4 +97,4 @@ with models.DAG(
     start_date=datetime.datetime(2024, 11, 15),
     catchup=False,
 ) as dag:
-  run_aotc_workload()
+    run_aotc_workload()
