c30d46745496a1640a084e77beac877be28d0f94
Quinn <qinyiyan@google.com>
2025-01-08 21:04:01 +0000

    Update microbenchmarks dag.
    Tested on ml-auto-solutions-dev.

--- dags/framework3p/microbenchmarks_dag.py
+++ dags/framework3p/microbenchmarks_dag.py
@@ -16,7 +16,17 @@ with models.DAG(
     start_date=datetime.datetime(2024, 9, 11),
     catchup=False,
 ) as dag:
-  all_gather_v4_16 = get_microbenchmark_config(
+
+  microbenchmarks_v4_8 = get_microbenchmark_config(
+      tpu_version=vm_resource.TpuVersion.V4,
+      tpu_cores=8,
+      tpu_zone=vm_resource.Zone.US_CENTRAL2_B,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.TPU_UBUNTU2204_BASE,
+      project=vm_resource.Project.CLOUD_ML_AUTO_SOLUTIONS,
+  )
+
+  microbenchmarks_v4_16 = get_microbenchmark_config(
       tpu_version=vm_resource.TpuVersion.V4,
       tpu_cores=16,
       tpu_zone=vm_resource.Zone.US_CENTRAL2_B,
@@ -25,7 +35,49 @@ with models.DAG(
       project=vm_resource.Project.CLOUD_ML_AUTO_SOLUTIONS,
   )
 
-  all_gather_v5e_4 = get_microbenchmark_config(
+  microbenchmarks_v4_64 = get_microbenchmark_config(
+      tpu_version=vm_resource.TpuVersion.V4,
+      tpu_cores=64,
+      tpu_zone=vm_resource.Zone.US_CENTRAL2_B,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.V2_ALPHA_TPUV5,
+      project=vm_resource.Project.CLOUD_ML_AUTO_SOLUTIONS,
+  )
+
+  microbenchmarks_v5p_8 = get_microbenchmark_config(
+      tpu_version=vm_resource.TpuVersion.V5P,
+      tpu_cores=8,
+      tpu_zone=vm_resource.Zone.US_EAST5_A,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.V2_ALPHA_TPUV5,
+      project=vm_resource.Project.TPU_PROD_ENV_AUTOMATED,
+      network=vm_resource.V5_NETWORKS,
+      subnetwork=vm_resource.V5P_SUBNETWORKS,
+  )
+
+  microbenchmarks_v5p_128 = get_microbenchmark_config(
+      tpu_version=vm_resource.TpuVersion.V5P,
+      tpu_cores=128,
+      tpu_zone=vm_resource.Zone.US_EAST5_A,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.V2_ALPHA_TPUV5,
+      project=vm_resource.Project.TPU_PROD_ENV_AUTOMATED,
+      network=vm_resource.V5_NETWORKS,
+      subnetwork=vm_resource.V5P_SUBNETWORKS,
+  )
+
+  microbenchmarks_v5p_256 = get_microbenchmark_config(
+      tpu_version=vm_resource.TpuVersion.V5P,
+      tpu_cores=256,
+      tpu_zone=vm_resource.Zone.US_EAST5_A,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.V2_ALPHA_TPUV5,
+      project=vm_resource.Project.TPU_PROD_ENV_AUTOMATED,
+      network=vm_resource.V5_NETWORKS,
+      subnetwork=vm_resource.V5P_SUBNETWORKS,
+  )
+
+  microbenchmarks_v5e_4 = get_microbenchmark_config(
       tpu_version=vm_resource.TpuVersion.V5E,
       tpu_cores=4,
       tpu_zone=vm_resource.Zone.US_EAST1_C,
@@ -36,16 +88,52 @@ with models.DAG(
       subnetwork=vm_resource.V5E_SUBNETWORKS,
   )
 
-  all_gather_v5e_256 = get_microbenchmark_xpk_config(
+  microbenchmarks_v5e_16 = get_microbenchmark_config(
       tpu_version=vm_resource.TpuVersion.V5E,
-      tpu_cores=256,
-      tpu_zone=vm_resource.Zone.US_WEST4_B,
+      tpu_cores=16,
+      tpu_zone=vm_resource.Zone.US_EAST1_C,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.TPU_VM_TF_NIGHTLY_POD,
+      project=vm_resource.Project.TPU_PROD_ENV_AUTOMATED,
+      network=vm_resource.V5_NETWORKS,
+      subnetwork=vm_resource.V5E_SUBNETWORKS,
+  )
+
+  microbenchmarks_v6e_4 = get_microbenchmark_config(
+      tpu_version=vm_resource.TpuVersion.TRILLIUM,
+      tpu_cores=4,
+      tpu_zone=vm_resource.Zone.US_EAST5_C,
+      time_out_in_min=60,
+      runtime_version=vm_resource.RuntimeVersion.V2_ALPHA_TPUV6,
+      project=vm_resource.Project.TPU_PROD_ENV_AUTOMATED,
+      network=vm_resource.V5_NETWORKS,
+      subnetwork=vm_resource.V6E_SUBNETWORKS,
+  )
+
+  microbenchmarks_v5e_256 = get_microbenchmark_xpk_config(
       time_out_in_min=60,
       test_name="framework-microbenchmark-v5e-256",
       docker_image=vm_resource.DockerImage.XPK_JAX_TEST.value,
       test_owner=test_owner.QINY_Y,
       cluster=vm_resource.XpkClusters.TPU_V5E_256_CLUSTER,
-      project=vm_resource.Project.TPU_PROD_ENV_MULTIPOD,
   ).run()
+
+  microbenchmarks_v6e_256 = get_microbenchmark_xpk_config(
+      time_out_in_min=60,
+      test_name="framework-microbenchmark-v6e-256",
+      docker_image=vm_resource.DockerImage.XPK_JAX_TEST.value,
+      test_owner=test_owner.QINY_Y,
+      cluster=vm_resource.XpkClusters.TPU_V6E_256_CLUSTER,
+  ).run()
+
+  microbenchmarks_v4_128 = get_microbenchmark_xpk_config(
+      time_out_in_min=60,
+      test_name="framework-microbenchmark-v4-128",
+      docker_image=vm_resource.DockerImage.XPK_JAX_TEST.value,
+      test_owner=test_owner.QINY_Y,
+      cluster=vm_resource.XpkClusters.TPU_V4_128_CLUSTER,
+  ).run()
+
+
 
 # Test dependency: run in parallel
