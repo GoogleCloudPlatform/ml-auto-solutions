076f56e833dbeed5bcb12bec521c9c358f04b355
HT.Guo <hengtaoguo@google.com>
2025-01-08 16:24:18 -0800

    Fix import error by using tf-nightly (#529)

--- dags/multipod/maxtext_profiling.py
+++ dags/multipod/maxtext_profiling.py
@@ -50,9 +50,8 @@ with models.DAG(
         f"gsutil cp -R {base_output_directory}/$RUN_NAME/tensorboard .",
         "pip3 uninstall -y tbp-nightly",
         "pip3 uninstall -y tensorboard_plugin_profile",
-        "pip3 install tensorboard_plugin_profile",
-        "python3 MaxText/tests/profiler_test.py",
-        "pip3 uninstall -y tensorboard_plugin_profile",
+        "pip3 uninstall -y tensorflow",
+        "pip3 install tf-nightly",
         "pip3 install tbp-nightly",
         "python3 MaxText/tests/profiler_test.py",
     )
