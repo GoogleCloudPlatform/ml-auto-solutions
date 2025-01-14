fe913098f1b0580a66822c63ec65fc788c5ba3b6
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-01-14 11:57:21 -0800

    Update run_mantaray_jobs.py

--- dags/mantaray/run_mantaray_jobs.py
+++ dags/mantaray/run_mantaray_jobs.py
@@ -41,7 +41,7 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
   # merge all PyTorch/XLA tests ino one Dag
   with models.DAG(
       dag_id="pytorch_xla_model_regression_test_on_trillium",
-      schedule="0 0 * * *",  # everyday at midnight # job["schedule"],
+      schedule="0 0 * * *" if composer_env.is_prod_env() else None,  # everyday at midnight # job["schedule"],
       tags=["mantaray", "pytorchxla", "xlml"],
       start_date=datetime.datetime(2024, 4, 22),
       catchup=False,
