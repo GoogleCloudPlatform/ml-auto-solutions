05ce67de40907bf03dc0ed47f5739a44e0a52bad
Daniel Li <dyli@google.com>
2025-01-16 12:49:31 -0800

    Reduce the number of request_rates

--- dags/solutions_team/solutionsteam_vllm_benchmarks.py
+++ dags/solutions_team/solutionsteam_vllm_benchmarks.py
@@ -42,7 +42,7 @@ with models.DAG(
           "backend": ["vllm"],
           "model_id": ["meta-llama/Meta-Llama-3.1-8B"],
           "dataset": "ShareGPT_V3_unfiltered_cleaned_split.json",
-          "request_rates": "1,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32",
+          "request_rates": "1,2,4,8,16,32",
           "num_prompts": 1000,
           "max_output_length": 1024,
           "max_cache_length": 2048,
