80b09ec29b475a1cfe30076f592ca1e030bae881
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-01-21 17:41:33 -0800

    Update run_mantaray_jobs.py

--- dags/mantaray/run_mantaray_jobs.py
+++ dags/mantaray/run_mantaray_jobs.py
@@ -33,14 +33,11 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
 
   # Create two DAG for PyTorch/XLA tests
   pattern = r"^(ptxla|pytorchxla_part1).*"
-  pattern2 = r"^(pytorchxla_part2).*"
   workload_file_name_list = []
   workload_file_name_list_2 = []
   for job in xlml_jobs:
     if re.match(pattern, job["task_name"]):
       workload_file_name_list.append(job["file_name"])
-    elif re.match(pattern2, job["task_name"]):
-      workload_file_name_list_2.append(job["file_name"])
 
   # merge all PyTorch/XLA tests ino one Dag
   with models.DAG(
@@ -49,24 +46,9 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
       tags=["mantaray", "pytorchxla", "xlml"],
       start_date=datetime.datetime(2024, 4, 22),
       catchup=False,
+      concurrency=1,
   ) as dag:
     for workload_file_name in workload_file_name_list:
-      run_workload = mantaray.run_workload.override(
-          task_id=workload_file_name.split(".")[0]
-      )(
-          workload_file_name=workload_file_name,
-      )
-      run_workload
-
-  # split out sd2 model test
-  with models.DAG(
-      dag_id="pytorch_xla_model_regression_test_on_trillium_share_zone_2",
-      schedule="0 0 * * *",  # everyday at midnight # job["schedule"],
-      tags=["mantaray", "pytorchxla", "xlml"],
-      start_date=datetime.datetime(2024, 4, 22),
-      catchup=False,
-  ) as dag:
-    for workload_file_name in workload_file_name_list_2:
       run_workload = mantaray.run_workload.override(
           task_id=workload_file_name.split(".")[0]
       )(
