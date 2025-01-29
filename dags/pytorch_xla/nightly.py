f1b963977c5223855859a0a9a54ef91d2aae468e
Yifei Teng <yifeit@google.com>
2025-01-28 17:52:07 -0800

    Add a multi-slice llama training test
    Specifically, two slices of v5p-8.
    
    This is to ensure that both r2.6 and ptxla nightly continue to work in
    multi-slice (DCN networking involved) environments.
    
    I noticed that we are not testing llama3 in nightly so I added that too,
    copying from our release tests.

--- dags/pytorch_xla/nightly.py
+++ dags/pytorch_xla/nightly.py
@@ -204,6 +204,33 @@ def llama():
       ),
       US_CENTRAL2_B,
   )
+  llama_3_train_trillium = task.run_queued_resource_test(
+      test_config.JSonnetTpuVmTest.from_pytorch(
+          "pt-nightly-llama3-train-func-v6e-4-1vm",
+          network=V5_NETWORKS,
+          subnetwork=V6E_SUBNETWORKS,
+      ),
+      US_CENTRAL2_B_TPU_PROD_ENV,
+  )
+  llama_3_train_v5p_2_slices = task.run_queued_resource_test(
+      test_config.JSonnetTpuVmTest.from_pytorch(
+          "pt-nightly-llama3-train-2-slice-func-v5p-8-1vm",
+          reserved=True,
+          network=V5_NETWORKS,
+          subnetwork=V5P_SUBNETWORKS,
+          num_slices=2,
+      ),
+      US_EAST5_A_TPU_PROD_ENV_AUTOMATED,
+  )
+  llama_3_train_v5p_8 = task.run_queued_resource_test(
+      test_config.JSonnetTpuVmTest.from_pytorch(
+          "pt-nightly-llama3-train-func-v5p-8-1vm",
+          reserved=True,
+          network=V5_NETWORKS,
+          subnetwork=V5P_SUBNETWORKS,
+      ),
+      US_EAST5_A_TPU_PROD_ENV_AUTOMATED,
+  )
 
 
 with models.DAG(
