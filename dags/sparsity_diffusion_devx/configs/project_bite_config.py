7a1c85ebc87d7b1ac362d9b085b2aafceda85f4a
asall <5251744+asall@users.noreply.github.com>
2025-03-10 14:50:58 -0700

    Added project_bite_tpu_unittests and associated config to test axlear… (#612)
    * Added project_bite_tpu_unittests and associated config to test axlearn framework with v6e TPUs
    
    - Currently only runs tests in axlearn/common/flash_attention/
    
    * Added pip freeze to TPU VM host
    Changed docker base image to python:3.10-slim
    
    * Install git manually, it's not included with new docker base image
    
    * Removed unnecessary JAX/TPUlib install on host
    
    * Fixed formatting issues detected by pyink

--- dags/sparsity_diffusion_devx/configs/project_bite_config.py
+++ dags/sparsity_diffusion_devx/configs/project_bite_config.py
@@ -92,3 +92,78 @@ def get_bite_tpu_config(
       task_test_config=job_test_config,
       task_gcp_config=job_gcp_config,
   )
+
+
+def get_bite_tpu_unittests_config(
+    tpu_version: TpuVersion,
+    tpu_cores: int,
+    tpu_zone: str,
+    runtime_version: str,
+    time_out_in_min: int,
+    task_owner: str,
+    is_tpu_reserved: bool = False,
+    pinned_version: Optional[str] = None,
+):
+  unittest_setupcmds = (
+      # create configuration files needed
+      """cat > Dockerfile_CI <<EOF
+FROM python:3.10-slim
+WORKDIR /workspace
+COPY run_tpu_tests.sh /workspace/
+RUN apt update -y
+RUN apt install -y git
+RUN git clone https://github.com/apple/axlearn.git
+WORKDIR /workspace/axlearn
+RUN pip install --upgrade pip
+RUN pip install -e '.[core,dev,gcp]'
+RUN pip install grain
+RUN pip install google-cloud-aiplatform
+RUN pip install -U --pre libtpu-nightly -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+RUN pip install --pre -U jaxlib -f https://storage.googleapis.com/jax-releases/jaxlib_nightly_releases.html
+RUN pip install git+https://github.com/google/jax
+RUN pip freeze
+EOF
+""",
+      # create script to run the tests inside of the container
+      # incluedes a basic sanity check python script which prints out TPU env info for reference
+      """cat > run_tpu_tests.sh <<EOF
+set -x
+echo '#### Starting TPU JAX Tests'
+pip freeze
+JAX_PLATFORMS='tpu' python -c 'import jax; jax.print_environment_info() ; print(f"Global device count: {jax.device_count()}")'
+cd /workspace/axlearn
+pytest --no-header -v axlearn/common/flash_attention/
+EOF
+""",
+      "chmod +x run_tpu_tests.sh",
+      "sudo docker build -f Dockerfile_CI -t ml-auto-solutions/tpu_unittests .",
+  )
+  # Run the unittest as non-root user, ulimit param req to mmap TPUs inside docker (default limit is 8192)
+  unittest_runcmds = (
+      "echo '#### Start docker image - tpu_unittests'",
+      "sudo docker run --network=host --privileged --ulimit memlock=-1:-1  ml-auto-solutions/tpu_unittests  /bin/bash -c '/workspace/run_tpu_tests.sh'",
+  )
+  job_gcp_config = gcp_config.GCPConfig(
+      project_name=Project.CLOUD_ML_AUTO_SOLUTIONS.value,
+      zone=tpu_zone,
+      dataset_name=metric_config.DatasetOption.XLML_DATASET,
+  )
+
+  tpu_unittests_test_config = test_config.TpuVmTest(
+      test_config.Tpu(
+          version=tpu_version,
+          cores=tpu_cores,
+          runtime_version=runtime_version,
+          reserved=is_tpu_reserved,
+      ),
+      test_name="bite_unittests",
+      set_up_cmds=unittest_setupcmds,
+      run_model_cmds=unittest_runcmds,
+      timeout=datetime.timedelta(minutes=time_out_in_min),
+      task_owner=task_owner,
+      gcs_subfolder=f"{GCS_SUBFOLDER_PREFIX}/jax",
+  )
+  return task.run_queued_resource_test(
+      task_test_config=tpu_unittests_test_config,
+      task_gcp_config=job_gcp_config,
+  )
