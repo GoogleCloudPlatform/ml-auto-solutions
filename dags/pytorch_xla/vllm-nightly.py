f71d664799253b542023a8e231c513b3641e9605
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-03-19 15:27:17 -0700

    Update vllm-nightly.py

--- dags/pytorch_xla/vllm-nightly.py
+++ dags/pytorch_xla/vllm-nightly.py
@@ -31,37 +31,39 @@ with models.DAG(
     start_date=datetime.datetime(2024, 1, 1),
     catchup=False,
 ) as dag:
-  # model = "all" if composer_env.is_prod_env() else "BERT_pytorch"
-  # torchbench_extra_flags = [f"--filter={model}"]
+    # follow example in https://github.com/GoogleCloudPlatform/ml-auto-solutions/blob/bda4d59ed7fd9dd3b244a8b2612385c4f5c9a8a9/dags/multipod/maxtext_gpu_end_to_end.py#L41
+    
 
-  # Running on V5P
-  config.get_torchbench_tpu_config(
-      tpu_version=resource.TpuVersion.V5P,
-      tpu_cores=8,
-      project=resource.Project.TPU_PROD_ENV_AUTOMATED,
-      tpu_zone=resource.Zone.US_EAST5_A,
-      runtime_version=resource.RuntimeVersion.V2_ALPHA_TPUV5,
-      network=resource.V5_NETWORKS,
-      subnetwork=resource.V5P_SUBNETWORKS,
-      time_out_in_min=1800,
-      model_name=model,
-      reserved=True,
-      preemptible=False,
-      extraFlags=" ".join(torchbench_extra_flags),
-  )
+    
+  # # Running on V6E
+  # config.get_torchbench_tpu_config(
+  #     tpu_version=resource.TpuVersion.TRILLIUM,
+  #     tpu_cores=8,
+  #     project=resource.Project.CLOUD_ML_BENCHMARKING,
+  #     tpu_zone=resource.Zone.US_CENTRAL2_B,
+  #     runtime_version=resource.RuntimeVersion.V2_ALPHA_TPUV6,
+  #     network=resource.BM_NETWORKS,
+  #     subnetwork=resource.V4_BM_SUBNETWORKS,
+  #     time_out_in_min=1600,
+  #     model_name=model,
+  #     reserved=False,
+  #     preemptible=False,
+  #     extraFlags=" ".join(torchbench_extra_flags),
+  # )
 
-  # Running on V6E
-  config.get_torchbench_tpu_config(
-      tpu_version=resource.TpuVersion.TRILLIUM,
-      tpu_cores=8,
-      project=resource.Project.CLOUD_ML_BENCHMARKING,
-      tpu_zone=resource.Zone.US_CENTRAL2_B,
-      runtime_version=resource.RuntimeVersion.V2_ALPHA_TPUV6,
-      network=resource.BM_NETWORKS,
-      subnetwork=resource.V4_BM_SUBNETWORKS,
-      time_out_in_min=1600,
-      model_name=model,
-      reserved=False,
-      preemptible=False,
-      extraFlags=" ".join(torchbench_extra_flags),
-  )
+
+# # merge all PyTorch/XLA tests ino one Dag
+# with models.DAG(
+#   dag_id="pytorch_xla_model_regression_test_on_trillium",
+#   schedule="0 0 * * *",  # everyday at midnight # job["schedule"],
+#   tags=["mantaray", "pytorchxla", "xlml"],
+#   start_date=datetime.datetime(2024, 4, 22),
+#   catchup=False,
+# ) as dag:
+# for workload_file_name in workload_file_name_list:
+#   run_workload = mantaray.run_workload.override(
+#       task_id=workload_file_name.split(".")[0]
+#   )(
+#       workload_file_name=workload_file_name,
+#   )
+#   run_workload
