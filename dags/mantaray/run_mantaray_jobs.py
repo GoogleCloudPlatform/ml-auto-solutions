dad22b33f514abf29a21cbef165dec200d9cae33
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2025-04-10 13:23:33 -0700

    Update run_mantaray_jobs.py

--- dags/mantaray/run_mantaray_jobs.py
+++ dags/mantaray/run_mantaray_jobs.py
@@ -125,36 +125,33 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
       test_config_meta = []
 
       test_config_meta.append(
-        bigquery.MetadataHistoryRow(
-          job_uuid=uuid,
-          metadata_key="accelerator",
-          metadata_value="v6e-4",
-        )
+          bigquery.MetadataHistoryRow(
+            job_uuid=uuid,
+            metadata_key="accelerator",
+            metadata_value="v6e-4",
+          )
       )
       test_config_meta.append(
-        bigquery.MetadataHistoryRow(
-          job_uuid=uuid,
-          metadata_key="project",
-          metadata_value=Project.CLOUD_ML_BENCHMARKING.value,
-        )
+          bigquery.MetadataHistoryRow(
+            job_uuid=uuid,
+            metadata_key="project",
+            metadata_value=Project.CLOUD_ML_BENCHMARKING.value,
+          )
       )
       if True:
         test_config_meta.append(
-          bigquery.MetadataHistoryRow(
-            job_uuid=uuid,
-            metadata_key="num_slices",
-            metadata_value=1,
-          )
+            bigquery.MetadataHistoryRow(
+              job_uuid=uuid,
+              metadata_key="num_slices",
+              metadata_value=1,
+            )
         )
         test_config_meta.append(
-          bigquery.MetadataHistoryRow(
-            job_uuid=uuid,
-            metadata_key="multislice_topology",
-            metadata_value=(
-              "1"
-              "v6e-4"
-            ),
-          )
+            bigquery.MetadataHistoryRow(
+              job_uuid=uuid,
+              metadata_key="multislice_topology",
+              metadata_value=("1" "v6e-4"),
+            )
         )
 
       metadata[index].extend(test_config_meta)
@@ -164,9 +161,9 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
 
   # generate metadata for test result to push to BigQuery Database
   def new_add_airflow_metadata(
-    base_id: str,
-    project_name: str,
-    metadata: List[List[bigquery.MetricHistoryRow]],
+      base_id: str,
+      project_name: str,
+      metadata: List[List[bigquery.MetricHistoryRow]],
   ) -> List[List[bigquery.MetricHistoryRow]]:
     """Add airflow metadata: run_id, prev_start_date_success,
     and airflow_dag_run_link.
@@ -184,25 +181,24 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
       airflow_meta = []
 
       airflow_meta.append(
-        bigquery.MetadataHistoryRow(
-          job_uuid=uuid, metadata_key="base_id", metadata_value=base_id
-        )
+          bigquery.MetadataHistoryRow(
+            job_uuid=uuid, metadata_key="base_id", metadata_value=base_id
+          )
       )
       metadata[index].extend(airflow_meta)
     return metadata
 
-
   # post-process to push test result to BigQuery Database via self-generated metadata
   @task_decorators
   def new_process_metrics(
-    base_id: str,
-    task_metric_config: Optional[metric_config.MetricConfig],
-    use_startup_script: bool = False,
-    folder_location: Optional[str] = None,
-    current_request_rate: int = 1,
+      base_id: str,
+      task_metric_config: Optional[metric_config.MetricConfig],
+      use_startup_script: bool = False,
+      folder_location: Optional[str] = None,
+      current_request_rate: int = 1,
   ) -> None:
     LOCAL_TPUVM_TPUVERSION_VALUE = TpuVersion.TRILLIUM.value
-    benchmark_id = f"reqeust-rate-{current_request_rate}-vllm-benchmark-tpu-nightly-LLaMA3.1-8B-v{LOCAL_TPUVM_TPUVERSION_VALUE}-4" # task_test_config.benchmark_id
+    benchmark_id = f"reqeust-rate-{current_request_rate}-vllm-benchmark-tpu-nightly-LLaMA3.1-8B-v{LOCAL_TPUVM_TPUVERSION_VALUE}-4"
     print("benchmark_id: ", benchmark_id)
     current_time = datetime.datetime.now()
     print("current_time: ", current_time)
@@ -217,32 +213,37 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
       if task_metric_config.json_lines:
         absolute_path = folder_location
         print("absolute_path: ", absolute_path)
-        metric_history_rows_list, metadata_history_rows_list = metric.process_json_lines(
-          base_id, absolute_path
-        )
+        (
+          metric_history_rows_list,
+          metadata_history_rows_list
+        ) = metric.process_json_lines(base_id, absolute_path)
 
     print("metric_history_rows_list: ", metric_history_rows_list)
     print("metadata_history_rows_list: ", metadata_history_rows_list)
 
     # add default airflow metadata
     metadata_history_rows_list = new_add_airflow_metadata(
-      base_id, Project.CLOUD_ML_AUTO_SOLUTIONS.value, metadata_history_rows_list
+        base_id,
+        Project.CLOUD_ML_AUTO_SOLUTIONS.value,
+        metadata_history_rows_list
     )
     print("metadata_history_rows_list: ", metadata_history_rows_list)
 
     metadata_history_rows_list = new_add_test_config_metadata(
-      base_id,
-      metadata_history_rows_list,
+        base_id,
+        metadata_history_rows_list,
     )
     print("metadata_history_rows_list: ", metadata_history_rows_list)
 
     test_run_rows = []
 
-    dataset_name = metric.update_dataset_name_if_needed(metric_config.DatasetOption.BENCHMARK_DATASET)
+    dataset_name = metric.update_dataset_name_if_needed(
+        metric_config.DatasetOption.BENCHMARK_DATASET
+    )
     print("dataset_name: ", dataset_name)
 
     bigquery_metric = bigquery.BigQueryMetricClient(
-      Project.CLOUD_ML_AUTO_SOLUTIONS.value, dataset_name
+        Project.CLOUD_ML_AUTO_SOLUTIONS.value, dataset_name
     )
     print("after bigquery_metric")
 
@@ -250,90 +251,93 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
       u_uuid = metric.generate_row_uuid(str(base_id), index)
       print("check current u_uuid: ", u_uuid)
       job_history_row = bigquery.JobHistoryRow(
-        uuid=u_uuid,
-        timestamp=current_time,
-        owner=test_owner.Team.PYTORCH_XLA.value,
-        job_name=benchmark_id,
-        job_status=bigquery.JobStatus.MISSED.value,
+          uuid=u_uuid,
+          timestamp=current_time,
+          owner=test_owner.Team.PYTORCH_XLA.value,
+          job_name=benchmark_id,
+          job_status=bigquery.JobStatus.MISSED.value,
       )
       test_run_row = bigquery.TestRun(
-        job_history_row,
-        metric_history_rows_list[index],
-        metadata_history_rows_list[index],
+          job_history_row,
+          metric_history_rows_list[index],
+          metadata_history_rows_list[index],
       )
       test_run_rows.append(test_run_row)
 
     print("Test run rows:", test_run_rows)
     bigquery_metric.insert(test_run_rows)
 
-
   HF_TOKEN_LLaMA3_8B = Variable.get("HF_TOKEN_LLaMA3_8B", None)
 
-
   # commands for vllm nightly benchmarking
-  def run_test_code_on_persistent_TPUVM(output_location: str, current_request_rate: int):
+  def run_test_code_on_persistent_TPUVM(
+      output_location: str, current_request_rate: int
+  ):
     """
     Run nightly vLLM inference benchmarking on persistent TPU.
     """
     print("output_location: ", output_location)
     print("current_request_rate: ", current_request_rate)
     gcloud_command = (
-      f"set -x && "
-      "set -u && "
-      "project=$(curl -sS \"http://metadata.google.internal/computeMetadata/v1/project/project-id\" -H \"Metadata-Flavor: Google\") && "
-      "zone=europe-west4-a && "
-      "tpu_name=manfei-2025-v6e-4-cloud-ml-auto-solu && "
-      "[ -f /scripts/id_rsa ] && sudo rm /scripts/id_rsa && sudo rm /scripts/id_rsa.pub; sudo ssh-keygen -t rsa -f /scripts/id_rsa -q -N \"\" && "
-      "echo \"xl-ml-test:$(cat /scripts/id_rsa.pub)\" > ssh-keys.txt && "
-      "echo 'echo Running startup script' > startup-script.txt && "
-      "sudo apt-get -y update && "
-      "sudo apt-get -y install lsof && "
-      "sudo dpkg --configure -a && "
-      "sudo apt-get -y install nfs-common && "
-      "yes '' | gcloud compute config-ssh && "
-      "ls /home/airflow/.ssh/ && "
-      "echo ${project} && "
-      "echo ${zone} && "
-      "echo ${tpu_name} && "
-      "yes 'y' | sudo gcloud alpha compute tpus tpu-vm ssh manfei-2025-v6e-4-cloud-ml-auto-solu --zone=europe-west4-a "
-      "--project=cloud-ml-auto-solutions --ssh-key-file=/home/airflow/.ssh/google_compute_engine --strict-host-key-checking=no "
-      "--internal-ip --worker=all --command ' \
-        sudo docker ps -a --filter \"name=testooo\" -q | grep -q . && sudo docker rm -f testooo && sudo docker image rmi us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:nightly_3.10_tpuvm; \
-        sudo docker run --privileged --net host --shm-size=16G --name testooo \
-        us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:nightly_3.10_tpuvm bash -c \" \
-          pip uninstall -y torch torchvision torch_xla jax jaxlib libtpu && \
-          git clone https://github.com/vllm-project/vllm.git && cd vllm && \
-          pip install -r requirements/tpu.txt && \
-          VLLM_TARGET_DEVICE=\'tpu\' python setup.py develop && \
-          export PJRT_DEVICE=TPU && \
-          export HF_TOKEN={HF_TOKEN_LLaMA3_8B} && \
-          VLLM_USE_V1=1 python -m vllm.entrypoints.openai.api_server --model meta-llama/Meta-Llama-3-8B --disable-log-requests \
-          --max-num-seq=320 --gpu-memory-utilization=0.95 --tensor-parallel-size=4 --max-model-len=8192 --port 8009 & sleep 800 && \
-          git clone -b inference-benchmark-script https://github.com/ManfeiBai/vllm.git vllmscript && "
-          f"bash vllmscript/benchmarks/inference_benchmark_script.sh {current_request_rate} && "
-          f"gsutil cp metric_result.jsonl {output_location} && ls \
-        \" && sudo docker stop testooo && sudo docker rm testooo && sudo docker image rmi us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:nightly_3.10_tpuvm' \
-      "
+        f"set -x && "
+        "set -u && "
+        "project=$(curl -sS \"http://metadata.google.internal/computeMetadata/v1/project/project-id\" -H \"Metadata-Flavor: Google\") && "
+        "zone=europe-west4-a && "
+        "tpu_name=manfei-2025-v6e-4-cloud-ml-auto-solu && "
+        "[ -f /scripts/id_rsa ] && sudo rm /scripts/id_rsa && sudo rm /scripts/id_rsa.pub; sudo ssh-keygen -t rsa -f /scripts/id_rsa -q -N \"\" && "
+        "echo \"xl-ml-test:$(cat /scripts/id_rsa.pub)\" > ssh-keys.txt && "
+        "echo 'echo Running startup script' > startup-script.txt && "
+        "sudo apt-get -y update && "
+        "sudo apt-get -y install lsof && "
+        "sudo dpkg --configure -a && "
+        "sudo apt-get -y install nfs-common && "
+        "yes '' | gcloud compute config-ssh && "
+        "ls /home/airflow/.ssh/ && "
+        "echo ${project} && "
+        "echo ${zone} && "
+        "echo ${tpu_name} && "
+        "yes 'y' | sudo gcloud alpha compute tpus tpu-vm ssh manfei-2025-v6e-4-cloud-ml-auto-solu --zone=europe-west4-a "
+        "--project=cloud-ml-auto-solutions --ssh-key-file=/home/airflow/.ssh/google_compute_engine --strict-host-key-checking=no "
+        "--internal-ip --worker=all --command ' \
+          sudo docker ps -a --filter \"name=testooo\" -q | grep -q . && sudo docker rm -f testooo && sudo docker image rmi us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:nightly_3.10_tpuvm; \
+          sudo docker run --privileged --net host --shm-size=16G --name testooo \
+          us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:nightly_3.10_tpuvm bash -c \" \
+            pip uninstall -y torch torchvision torch_xla jax jaxlib libtpu && \
+            git clone https://github.com/vllm-project/vllm.git && cd vllm && \
+            pip install -r requirements/tpu.txt && \
+            VLLM_TARGET_DEVICE=\'tpu\' python setup.py develop && \
+            export PJRT_DEVICE=TPU && \
+            export HF_TOKEN={HF_TOKEN_LLaMA3_8B} && \
+            VLLM_USE_V1=1 python -m vllm.entrypoints.openai.api_server --model meta-llama/Meta-Llama-3-8B --disable-log-requests \
+            --max-num-seq=320 --gpu-memory-utilization=0.95 --tensor-parallel-size=4 --max-model-len=8192 --port 8009 & sleep 800 && \
+            git clone -b inference-benchmark-script https://github.com/ManfeiBai/vllm.git vllmscript && "
+            f"bash vllmscript/benchmarks/inference_benchmark_script.sh {current_request_rate} && "
+            f"gsutil cp metric_result.jsonl {output_location} && ls \
+          \" && sudo docker stop testooo && sudo docker rm testooo && sudo docker image rmi us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/xla:nightly_3.10_tpuvm' \
+        "
     )
     return gcloud_command
 
-
   # execute commands for vllm nightly benchmarking
   @task_decorators
-  def run_on_v6e_4_persistant_TPUVM(output_location: str, current_request_rate: int):
+  def run_on_v6e_4_persistant_TPUVM(
+      output_location: str, current_request_rate: int
+  ):
     with tempfile.TemporaryDirectory() as tmpdir:
       print("output_location: ", output_location)
       hook = SubprocessHook()
       result = hook.run_command(
-        [
-          "bash",
-          "-c",
-          run_test_code_on_persistent_TPUVM(output_location, current_request_rate),
-        ],
-        cwd=tmpdir,
+          [
+              "bash",
+              "-c",
+              run_test_code_on_persistent_TPUVM(
+                output_location,
+                current_request_rate
+              ),
+          ],
+          cwd=tmpdir,
       )
       print("Command finished with code: {result.exit_code}")
-
 
   # generated GCS path for vllm nightly benchmark test result
   @task_decorators
@@ -344,7 +348,6 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
     output_location = f"gs://ml-auto-solutions/output/pytorch-xla/vllm_benchmark_nightly/vllm-nightly-v6e-8-{current_datetime}-{current_request_rate}/metric_report.jsonl"
     return output_location
 
-
   # call execute commands for vllm nightly benchmark test result, and push test result to BigQuery Database in post-process
   @task_group(prefix_group_id=False)
   def run_vllm_nightly_on_v6e_4_persistant_TPUVM(current_request_rate: int):
@@ -353,67 +356,72 @@ if composer_env.is_prod_env() or composer_env.is_dev_env():
     print("output_location: ", output_location)
 
     ### run test code
-    run_on_v6e_4_persistant_TPUVM_func = run_on_v6e_4_persistant_TPUVM(output_location, current_request_rate)
+    run_on_v6e_4_persistant_TPUVM_func = run_on_v6e_4_persistant_TPUVM(
+        output_location, current_request_rate
+    )
 
     ### push test result to BigQuery Database
     ### task_metric_config: generate metric configs to push test result
     job_metric_config = metric_config.MetricConfig(
-      json_lines="metric_report.jsonl",
-      use_runtime_generated_gcs_folder=True,
+        json_lines="metric_report.jsonl",
+        use_runtime_generated_gcs_folder=True,
     )
 
     process_id = metric.generate_process_id.override(retries=0)()
     print("process_id: ", process_id)
     post_process = new_process_metrics(
-      str(process_id),
-      job_metric_config,
-      folder_location=output_location,
+        str(process_id),
+        job_metric_config,
+        folder_location=output_location,
     )
 
     ### order of execute
     run_on_v6e_4_persistant_TPUVM_func >> post_process
-
 
   # split func defination with different func name to show on Airflow Dashboard
   @task_group(prefix_group_id=False)
   def vllm_request_rate_1_nightly_on_v6e_4_persistant_TPUVM():
     run_vllm_nightly_on_v6e_4_persistant_TPUVM(1)
 
-
   @task_group(prefix_group_id=False)
   def vllm_request_rate_4_nightly_on_v6e_4_persistant_TPUVM():
     run_vllm_nightly_on_v6e_4_persistant_TPUVM(4)
-
 
   @task_group(prefix_group_id=False)
   def vllm_request_rate_16_nightly_on_v6e_4_persistant_TPUVM():
     run_vllm_nightly_on_v6e_4_persistant_TPUVM(16)
 
-
   @task_group(prefix_group_id=False)
   def vllm_request_rate_inf_nightly_on_v6e_4_persistant_TPUVM():
-    run_vllm_nightly_on_v6e_4_persistant_TPUVM(0) # use 0 to present inf in this program
+    run_vllm_nightly_on_v6e_4_persistant_TPUVM(
+        0
+    ) # use 0 to present inf in this program
 
 
   # merge all PyTorch/XLA tests ino one Dag
   with models.DAG(
-    dag_id="pytorch_xla_model_regression_test_on_trillium",
-    schedule="0 0 * * *",
-    tags=["mantaray", "pytorchxla", "xlml"],
-    start_date=datetime.datetime(2024, 4, 22),
-    catchup=False,
+      dag_id="pytorch_xla_model_regression_test_on_trillium",
+      schedule="0 0 * * *",
+      tags=["mantaray", "pytorchxla", "xlml"],
+      start_date=datetime.datetime(2024, 4, 22),
+      catchup=False,
   ) as dag:
     # Training model(LLaMA3-8B, LLaMA3-70B, Mixtral8_7B, SD2)
     for workload_file_name in workload_file_name_list:
       run_workload = mantaray.run_workload.override(
-        task_id=workload_file_name.split(".")[0]
+          task_id=workload_file_name.split(".")[0]
       )(
-        workload_file_name=workload_file_name,
+          workload_file_name=workload_file_name,
       )
       run_workload
 
     # vLLM nightly Benchmark via inference_benchmark github repo script
-    vllm_request_rate_1_nightly_on_v6e_4_persistant_TPUVM() >> vllm_request_rate_4_nightly_on_v6e_4_persistant_TPUVM() >> vllm_request_rate_16_nightly_on_v6e_4_persistant_TPUVM() >> vllm_request_rate_inf_nightly_on_v6e_4_persistant_TPUVM()
+    (
+        vllm_request_rate_1_nightly_on_v6e_4_persistant_TPUVM()
+        >> vllm_request_rate_4_nightly_on_v6e_4_persistant_TPUVM()
+        >> vllm_request_rate_16_nightly_on_v6e_4_persistant_TPUVM()
+        >> vllm_request_rate_inf_nightly_on_v6e_4_persistant_TPUVM()
+    )
 
 
   # Create a DAG for each job from maxtext
