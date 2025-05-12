7c03d38373244d4ace6e6ab60138ba5f37bab016
Yijia <jinyijia24@gmail.com>
2025-05-12 12:03:17 -0700

    nit

--- dags/map_reproducibility/utils/common_utils_inference.py
+++ dags/map_reproducibility/utils/common_utils_inference.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
-import bigquery
 import jsonlines, json
 from google.cloud.bigquery import Client, LoadJobConfig, SourceFormat, SchemaField
 
