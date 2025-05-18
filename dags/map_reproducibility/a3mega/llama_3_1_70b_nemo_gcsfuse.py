5ced8941827b915382456989fcb1fb3cbac8d137
lepan-google <lepan@google.com>
2025-05-18 05:56:07 +0000

    [GCS Regression Test] Add supports for A3Mega llama3.1 70B NeMo GCSfuse recipe in main branch

--- dags/map_reproducibility/a3mega/llama_3_1_70b_nemo_gcsfuse.py
+++ dags/map_reproducibility/a3mega/llama_3_1_70b_nemo_gcsfuse.py
@@ -22,20 +22,23 @@ from dags.map_reproducibility.utils.common_utils import run_nemo_workload
 
 MODEL_ID = "llama3-1-70b"
 METRICS_MODEL_ID = "llama3.1-70b"
-CONFIG_MODEL_NAME = "llama3.1-70b-256gpus-bf16-gcsfuse-checkpointing.yaml"
 PRECISION = "bf16"
 HYPERCOMPUTER = "a3mega"
 FRAMEWORK = "nemo"
 KUEUE_NAME = "multislice-kueue"
 STORAGE_PRODUCT = "gcs"
 CHECKPOINT_BUCKET = "cmcs-checkpoint-sydney"
-DATASET_BUCKET = "cmcs-benchmark-raw-metrics"
+DATASET_BUCKET = "cmcs-storage-training-benchmark"
 GCS_METRICS_BUCKET = "cmcs-benchmark-raw-metrics"
 LOGS_BUCKET = "cmcs-benchmark-logs"
 BENCHMARK_TYPE = "checkpointing"
 WORKLOAD_TYPE = "system"
 WORKLOAD_IMAGE = "us-docker.pkg.dev/supercomputer-testing/dlsl-metadata/recipe-release-patched"
-RECIPE_BRANCH = "storage-next"
+# Please choose the following values based on the recipe branch.
+CONFIG_MODEL_NAME_MAIN_BRANCH = "llama3-1-70b-256gpus-bf16-pile-checkpointing.yaml"
+CONFIG_MODEL_NAME_STORAGE_NEXT_BRANCH = "llama3.1-70b-256gpus-bf16-gcsfuse-checkpointing.yaml"
+HELM_TEMPLATE_FOLDER_NAME_MAIN_BRANCH = "nemo-training-v2"
+STORAGE_NEXT_BRANCH = "storage-next"
 
 default_dag_args = {
     "retries": 0,
@@ -56,20 +59,26 @@ with models.DAG(
       precision=PRECISION,
       kueue_name=KUEUE_NAME,
       metrics_model_id=METRICS_MODEL_ID,
-      config_model_name=CONFIG_MODEL_NAME,
-      user="",
-      git_name="",
-      git_email="",
+      config_model_name=CONFIG_MODEL_NAME_STORAGE_NEXT_BRANCH,
+      user="lepan",
+      git_name="lepan",
+      git_email="lepan@google.com",
       gcs_results_generator=True,
       storage_product=STORAGE_PRODUCT,
-      recipe_branch=RECIPE_BRANCH,
-      recipes_repo_change_refs="",
+      # recipe_branch="",
+      recipe_branch=STORAGE_NEXT_BRANCH,
+      # recipes_repo_change_refs="refs/changes/20/4120/1",
+      recipes_repo_change_refs="refs/changes/40/4140/1",
       bq_writer_repo_change_refs="",
-      gcs_automation_repo_change_refs="",
+      gcs_automation_repo_change_refs="refs/changes/81/2081/1",
       gcs_source_bucket=CHECKPOINT_BUCKET,
       gcs_metrics_bucket=GCS_METRICS_BUCKET,
       benchmark_type=BENCHMARK_TYPE,
       logs_bucket=LOGS_BUCKET,
+      checkpoint_bucket=CHECKPOINT_BUCKET,
+      dataset_bucket=DATASET_BUCKET,
       workload_type=WORKLOAD_TYPE,
       workload_image=WORKLOAD_IMAGE,
+      # Leave helm_template_folder empty if using storage-next branch.
+      # helm_template_folder=HELM_TEMPLATE_FOLDER,
   )
--- dags/map_reproducibility/utils/gcs_automation_utils.py
+++ dags/map_reproducibility/utils/gcs_automation_utils.py
@@ -93,10 +93,10 @@ def _logs_scraper_cmds(
       A command to run GCS log scraper.
   """
   cmds = (
-      "python3 "
-      f"{recipe_repo_root}/src/utils/checkpointing_metrics"
+      f"(cd {recipe_repo_root} && git checkout storage-next "
+      f"&& python3 {recipe_repo_root}/src/utils/checkpointing_metrics"
       f"/log_scraper_nemo.py --logs_bucket={logs_bucket} "
-      f"--raw_metrics_bucket={gcs_metrics_bucket}",
+      f"--raw_metrics_bucket={gcs_metrics_bucket})",
   )
   return cmds
 
@@ -135,3 +135,118 @@ def _run_results_generator_cmds(
 
   cmds += (metrics_cmds,)
   return cmds
+
+
+def pv_pvc_generate_cmds(
+    storage_product: str,
+    recipe_branch: str,
+    dataset_bucket: str,
+    checkpoint_bucket: str,
+    recipe_repo_root: str,
+    user: str = None,
+):
+  """Get the commands for PV and PVC generaton for storage recipe in
+  main branch.
+
+  Args:
+      storage_product: The name of the storage product.
+      recipe_branch: The branch of the recipe repo.
+      dataset_bucket: The bucket which holds training dataset.
+      checkpoint_bucket: The bucket which holds the checkpoint files.
+      recipe_repo_root: The root path of the recipe repo.
+      user: The user name.
+
+  Returns:
+      A command to generate PV and PVC.
+  """
+  if not (storage_product and recipe_branch in ("main", "", None)):
+    return ()
+  helm_template_folder = pv_pvc_helm_template_folder(storage_product)
+  cmds = (
+      f"helm install -f {recipe_repo_root}/src/helm-charts/storage/"
+      f"{helm_template_folder}/values.yaml "
+      "--set gcsVolumes[0].name=gcs-data-$NOW "
+      f"--set gcsVolumes[0].bucketName={dataset_bucket} "
+      "--set gcsVolumes[1].name=gcs-checkpoints-$NOW "
+      f"--set gcsVolumes[1].bucketName={checkpoint_bucket} "
+      f"{f'{user}-' if user else ''}{storage_product}-$NOW-pv-pvc "
+      f"{recipe_repo_root}/src/helm-charts/storage/{helm_template_folder}",
+  )
+  return cmds
+
+
+def pv_pvc_cleanup_cmds(
+    storage_product: str = None,
+    recipe_branch: str = None,
+    user: str = None,
+):
+  """Get the commands for PV and PVC cleanup for storage recipe in
+  main branch.
+
+  Args:
+      storage_product: The name of the storage product.
+      recipe_branch: The branch of the recipe repo.
+      user: The user name.
+
+  Returns:
+      A command to clean up PV and PVC.
+  """
+  if not (storage_product and recipe_branch in ("main", "", None)):
+    return ()
+  cmds = (
+      f"helm uninstall "
+      f"{f'{user}-' if user else ''}{storage_product}-$NOW-pv-pvc",
+  )
+  return cmds
+
+
+def pv_pvc_workload_opts(
+    storage_product: str = None,
+    recipe_branch: str = None,
+    logs_bucket: str = None,
+    dataset_bucket: str = None,
+    checkpoint_bucket: str = None,
+):
+  """Get the PV and PVC setup options which will be appended to the workload
+  commands.
+
+  Args:
+      storage_product: The name of the storage product.
+      recipe_branch: The branch of the recipe repo.
+      logs_bucket: The bucket which holds the workload logs.
+      dataset_bucket: The bucket which holds the training dataset.
+      checkpoint_bucket: The bucket which holds the checkpoint files.
+
+  Returns:
+      The PV and PVC setup options which will be appended to the workload
+      commnads.
+  """
+  if not storage_product:
+    return ""
+  if storage_product != "gcs":
+    raise ValueError(f"{storage_product} is not supported.")
+  if recipe_branch in ("main", "", None):
+    return (
+        f" --set volumes.gcsMounts[0].bucketName={logs_bucket}"
+        " --set volumes.pvcMounts[0].claimName=gcs-data-$NOW-pvc"
+        " --set volumes.pvcMounts[1].claimName=gcs-checkpoints-$NOW-pvc"
+    )
+  elif recipe_branch == "storage-next":
+    return (
+        " --set volumes.gcsVolumes[0].name=gcs-data-$NOW"
+        f" --set volumes.gcsVolumes[0].bucketName={dataset_bucket}"
+        " --set volumes.gcsVolumes[1].name=gcs-checkpoints-$NOW"
+        f" --set volumes.gcsVolumes[1].bucketName={checkpoint_bucket}"
+        " --set volumes.gcsVolumes[2].name=gcs-logs-$NOW"
+        f" --set volumes.gcsVolumes[2].bucketName={logs_bucket}"
+    )
+  else:
+    raise ValueError(f"{recipe_branch} recipe branch is not supported.")
+
+
+def pv_pvc_helm_template_folder(storage_product: str):
+  if storage_product == "gcs":
+    return "gcs-fuse"
+  else:
+    raise ValueError(
+        f"{storage_product} is not supported, no helm chart configs are found.")
