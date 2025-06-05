bd9832f22451889f01687d5ba65fb06171a98c33
Ernie Chang <changernie@google.com>
2025-06-05 18:45:00 +0800

    [UPDATE] Update format and fix path

--- dags/multipod/maxtext_multi_tier_p2_chkpt_save_gcs.py
+++ dags/multipod/maxtext_multi_tier_p2_chkpt_save_gcs.py
@@ -37,7 +37,7 @@ with models.DAG(
   ram_disk = "/local"
   test_configs = {"v5p-8": [2]}
   clusters = {"v5p-8": XpkClusters.TPU_V5P_8_CLUSTER}
-  step = 200
+  step = 500
   local_checkpoint_period = 20
   replicator_backup_interval_minutes = "1"
   use_replicator = "True"
