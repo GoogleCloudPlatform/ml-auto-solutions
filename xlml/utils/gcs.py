1db48b65f6ab0aeca9cb5a6868df11c8166fc0e9
ernie-chang <ernie.chang@cienet.com>
2025-06-06 11:52:09 +0800

    Update gcs.py with more flexible

--- xlml/utils/gcs.py
+++ xlml/utils/gcs.py
@@ -3,7 +3,7 @@ import re
 from absl import logging
 
 
-def validate_gcs_checkpoint_p2(output_path):
+def validate_gcs_checkpoint(output_path):
   hook = GCSHook()
   pattern = re.compile(r"^gs://(?P<bucket>[^/]+)/(?P<prefix>.+)$")
   m = pattern.match(output_path)
@@ -14,8 +14,4 @@ def validate_gcs_checkpoint_p2(output_path):
   logging.info(f"prefix:{prefix}")
   files = hook.list(bucket_name=bucket_name, prefix=prefix)
   logging.info("Files ===> ", files)
-  if len(files) > 0:
-    for file in files:
-      if ".data" in file:
-        return True
-  return False
+  return files
