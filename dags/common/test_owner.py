da067f66a9167613474bf68419229a41304da1f0
severusho <severus.ho@cienet.com>
2025-08-18 18:38:01 +0800

    add Rohan's username

--- dags/common/test_owner.py
+++ dags/common/test_owner.py
@@ -75,7 +75,7 @@ PARAM_B = "parambole"
 KUNJAN_P = "coolkp"
 MICHELLE_Y = "michelle-yooh"
 SHUNING_J = "shuningjin"
-ROHAN_B = "Rohan B."
+ROHAN_B = "Rohan-Bierneni"
 
 # Inference
 ANDY_Y = "Andy Y."
