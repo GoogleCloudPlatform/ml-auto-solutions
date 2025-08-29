64fb6fe6b20ab0a1ddd4170a667acf06f6af1a24
juliekuo <juliekuo@google.com>
2025-08-29 16:43:32 +0800

    dag ui params demo

---
+++ dags/maxtext_pw/configs/model_configs.py
@@ -0,0 +1,11 @@
+MODEL_FRAMEWORK = [
+  'mcjax',
+  'pathways'
+]
+
+MODEL_NAME = [
+  'llama3_1_8b_8192',
+  'llama3_1_70b_8192',
+  'llama3_1_405b_8192_fsdp_dcn',
+  'llama2_70b_4096_real_data_long_run'
+]
