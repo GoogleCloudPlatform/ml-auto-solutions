6c8b0083f75c1842856d8ef0fdb2d391c85054e2
juliekuo <juliekuo@google.com>
2025-09-11 16:17:56 +0800

    add pw_mcjax_benchmark_recipe_dag.py and other configurations

---
+++ dags/maxtext_pathways/configs/parameters.py
@@ -0,0 +1,121 @@
+from airflow.models.param import Param
+from dags.maxtext_pathways.configs.model_configs import MaxTextV5eModelConfigs, MaxTextV5pModelConfigs, MaxTextTrilliumModelConfigs
+from dags.common.vm_resource import TpuVersion
+
+MODEL_FRAMEWORK = [
+  'mcjax',
+  'pathways'
+]
+
+MODEL_NAME = []
+V5E_MODEL_NAME = [config.value for config in MaxTextV5eModelConfigs]
+V5P_MODEL_NAME = [config.value for config in MaxTextV5pModelConfigs]
+V6E_MODEL_NAME = [config.value for config in MaxTextTrilliumModelConfigs]
+MODEL_NAME.extend(V5E_MODEL_NAME)
+MODEL_NAME.extend(V5P_MODEL_NAME)
+MODEL_NAME.extend(V6E_MODEL_NAME)
+
+DEVICE_VERSION = ['v' + version.value for version in TpuVersion]
+
+PARAMETERS={
+    'user': Param(
+      'user_name',
+      type='string',
+      title='User',
+      description='User name.'
+    ),
+    'cluster_name': Param(
+      'pw-scale-test-v5e-32',
+      type='string',
+      title='Cluster Name',
+      description='GKE/GCP cluster name.'
+    ),
+    'project': Param(
+      'cloud-tpu-multipod-dev',
+      type='string',
+      title='Project',
+      description='GCP project ID.'
+    ),
+    'zone': Param(
+      'us-south1-a',
+      type='string',
+      title='Zone',
+      description='GCP zone.'
+    ),
+    'device_version': Param(
+      'v5litepod',
+      type='string',
+      title='Device Version',
+      description='Device type for the cluster. ex: "v5litepod"-32',
+      enum=DEVICE_VERSION
+    ),
+    'core_count': Param(
+      32,
+      type='integer',
+      title='Core Count',
+      description='Device core count for the cluster. ex: v5litepod-"32"'
+    ),
+    'benchmark_steps': Param(
+      20,
+      type='integer',
+      title='Benchmark Steps',
+      description='Number of benchmark steps.'
+    ),
+    'num_slices_list': Param(
+      2,
+      type='string',
+      title='Num Slices List',
+      description='List of number of slices. ex: "2,4"'
+    ),
+    'server_image': Param(
+      'us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/unsanitized_server:latest',
+      type='string',
+      title='Server Image',
+      description='Server image for the cluster.'
+    ),
+    'proxy_image': Param(
+      'us-docker.pkg.dev/cloud-tpu-v2-images-dev/pathways/unsanitized_proxy_server:latest',
+      type='string',
+      title='Proxy Image',
+      description='Proxy image for the cluster.'
+    ),
+    'runner': Param(
+      'gcr.io/tpu-prod-env-one-vm/chzheng_latest:latest',
+      type='string',
+      title='Runner Image',
+      description='Runner image for the cluster.'
+    ),
+    'selected_model_framework': Param(
+      MODEL_FRAMEWORK[1],
+      type='string',
+      title='Model Framework',
+      description='Select a model framework to run.',
+      enum=MODEL_FRAMEWORK # dropdown menu
+    ),
+    'selected_model_names': Param(
+      'llama3_1_8b_8192_v5e_256',
+      type='string',
+      title='Model Name',
+      description='Select a model name to run.',
+      enum=MODEL_NAME
+    ),
+    'priority': Param(
+      'medium',
+      type='string',
+      title='Priority',
+      description='Priority for the workload',
+      enum=['very high', 'high', 'medium', 'low']
+    ),
+    'max_restarts': Param(
+      1,
+      type='integer',
+      title='Max Restarts',
+      description='Max restarts for the workload'
+    ),
+    # 'time_out_in_min': Param(
+    #   60,
+    #   type='integer',
+    #   title='Time Out In Minutes',
+    #   description='Time out in minutes for the workload task, adjust it when your meet (airflow.exceptions.AirflowSensorTimeout: Sensor has timed out).'
+    # ),
+  }
---
+++ dags/maxtext_pathways/pw_mcjax_benchmark_recipe_dag.py
@@ -0,0 +1,222 @@
+import datetime
+import os
+import tempfile
+from absl import logging
+from airflow.decorators import task
+from airflow.exceptions import AirflowFailException
+from airflow.hooks.subprocess import SubprocessHook
+from airflow.models.dag import DAG
+from airflow.operators.python import get_current_context
+from dags.common.vm_resource import DockerImage
+from dags.maxtext_pathways.configs.commands import ENV_COMMAND, RECIPE_COMMAND
+from dags.maxtext_pathways.configs.parameters import PARAMETERS
+from xlml.utils.xpk import _get_core_api_client, _list_workload_pods, _get_batch_api_client, _get_workload_job, LOGGING_URL_FORMAT
+
+with DAG(
+  dag_id="pw_mcjax_benchmark_recipe_dag",
+  start_date=datetime.datetime(2025, 1, 1),
+  schedule_interval=None,
+  catchup=False,
+  tags=[
+    'maxtext',
+    'pathways',
+    'mcjax',
+    'benchmark'
+    'nightly',
+  ],
+  description="A DAG to run a MaxText pw_mcjax_benchmark_recipe on GKE.",
+  params=PARAMETERS
+) as dag:
+  @task
+  def build_recipe_command():
+    ''' Combine preset commands and UI parameters. '''
+    # Get the DAG run's configuration and parameters.
+    context = get_current_context()
+    params = context.get('params', {})
+
+    # Initialization command.
+    recipe_cmds = RECIPE_COMMAND[0]
+
+    # Combine command.
+    device_type = params['device_version'] + '-' + str(params['core_count'])
+    recipe_cmds += f' --device_type={device_type}'
+
+    for key, value in params.items():
+      if key not in ['time_out_in_min', 'device_version', 'core_count']:
+        if isinstance(value, int):
+          recipe_cmds += f' --{key}={value}'
+        else:
+          recipe_cmds += f" --{key}='{value}'"
+          
+    logging.info('\n' + recipe_cmds.replace(' --', '\n  --'))
+    
+    return (recipe_cmds,)
+
+  @task
+  def run_workload(recipe_cmds):
+    """Run workload through xpk tool."""
+
+    context = get_current_context()
+    params = context.get('params', {})
+
+    container_workload_cmds = ENV_COMMAND[0] + ' && ' + recipe_cmds[0]
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+      xpk_cmds = [
+        "set -xue",
+        "git clone --branch v0.4.1 https://github.com/AI-Hypercomputer/xpk"
+        f" {tmpdir}/xpk",
+        "pip install ruamel.yaml docker",
+      ]
+  
+      workload_create_cmds = [
+        f"python {tmpdir}/xpk/xpk.py workload create"
+        f" --cluster={params['cluster_name']} --workload={params['user']}-workload"
+        f" --command='{container_workload_cmds}' --device-type={params['device_version']}-{str(params['core_count'])}"
+        f" --num-slices=1 --docker-image={DockerImage.MAXTEXT_TPU_JAX_NIGHTLY.value}"
+        f" --project={params['project']} --zone={params['zone']}"
+        " --restart-on-user-code-failure",
+      ]
+
+      cmds = []
+      cmds.extend(xpk_cmds)
+      cmds.extend(workload_create_cmds)
+
+      logging.info(f'{cmds = }')
+      logging.info(f'{xpk_cmds = }')
+      logging.info(f'{workload_create_cmds = }')
+      logging.info(f'{container_workload_cmds = }')
+      logging.info(f'{ENV_COMMAND = }')
+      logging.info(f'{recipe_cmds = }')
+
+      hook = SubprocessHook()
+      result = hook.run_command(
+          ["bash", "-c", ";".join(cmds)],
+          env={**os.environ, "KUBECONFIG": os.path.join(tmpdir, "xpk.conf")},
+      )
+      assert (
+          result.exit_code == 0
+      ), f"XPK command failed with code {result.exit_code}"
+
+
+  @task.sensor(poke_interval=60, timeout=3600, mode="reschedule")
+  def wait_for_workload_completion() -> bool:
+    """Check the workload status."""
+
+    context = get_current_context()
+    params = context.get('params', {})
+
+    workload_id = f"{params['user']}-workload"
+    project_id = params['project']
+    region = params['zone'][:-2]
+    cluster_name = params['cluster_name']
+
+    core_api = _get_core_api_client(project_id, region, cluster_name)
+    pods = _list_workload_pods(core_api, workload_id)
+
+    if not pods.items:
+      logging.info(f"No pods found for workload selector: {workload_id}.")
+
+      # Pathways jobs delete all pods on failure so we must also check if the job
+      # is complete
+      batch_api = _get_batch_api_client(project_id, region, cluster_name)
+      job = _get_workload_job(batch_api, workload_id)
+      if job is None:
+        logging.info(
+            f"No pods or jobs were found for workload selector: {workload_id}"
+        )
+        return False
+
+      if any(condition.type == "Failed" for condition in job.status.conditions):
+        # Don't keep retrying if the job has failed
+        raise AirflowFailException('Job has condition type: "Failed"')
+
+      if any(condition.type == "Complete" for condition in job.status.conditions):
+        logging.info(
+            "No pods found but job is complete for workload selector:"
+            f" {workload_id}"
+        )
+        return True
+
+      return False
+
+    if any(pod.status.phase in ["Pending", "Running"] for pod in pods.items):
+      logging.info("At least one pod has yet to complete.")
+      return False
+
+    try:
+      for pod in pods.items:
+        if pod.status.phase == "Failed":
+          # Don't keep retrying if the pod has failed
+          raise AirflowFailException(f"Bad pod phase: {pod.status.phase}")
+        elif pod.status.phase in ["Unknown"]:
+          raise RuntimeError(f"Bad pod phase: {pod.status.phase}")
+    finally:
+      # TODO(jonbolin): log printing for GPUs, which have multiple containers
+      if len(pod.spec.containers) == 1:
+        # Print the logs of the last pod checked - either the first failed pod or
+        # the last successful one.
+        logs = core_api.read_namespaced_pod_log(
+            name=pod.metadata.name, namespace=pod.metadata.namespace
+        )
+        logging.info(f"Logs for pod {pod.metadata.name}:")
+        for line in logs.split("\n"):
+          logging.info(line)
+      url = LOGGING_URL_FORMAT.format(
+          project=project_id,
+          region=region,
+          cluster=cluster_name,
+          workload_id=workload_id,
+      )
+      logging.info(f"Link to logs: {url}")
+
+    logging.info("All pod(s) phase are succeeded.")
+    return True
+  
+  @task(trigger_rule="all_done")
+  def clean_up_workload():
+    """Delete workload."""
+
+    context = get_current_context()
+    params = context.get('params', {})
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+      xpk_cmds = [
+        "set -xue",
+        "git clone --branch v0.4.1 https://github.com/AI-Hypercomputer/xpk"
+        f" {tmpdir}/xpk",
+        "pip install ruamel.yaml docker",
+      ]
+
+      workload_delete_cmd = [
+          f"python {tmpdir}/xpk/xpk.py workload delete"
+          f" --cluster={params['cluster_name']} --workload={params['user']}-workload"
+          f" --project={params['project']} --zone={params['zone']}",
+      ]
+
+      cmds = []
+      cmds.extend(xpk_cmds)
+      cmds.extend(workload_delete_cmd)
+
+      hook = SubprocessHook()
+      result = hook.run_command(
+          ["bash", "-c", ";".join(cmds)],
+          env={**os.environ, "KUBECONFIG": os.path.join(tmpdir, "xpk.conf")},
+      )
+      assert (
+          result.exit_code == 0
+      ), f"XPK clean-up failed with code {result.exit_code}"
+
+   # Define task dependencies by instantiating and linking tasks.
+  build_recipe_command_task = build_recipe_command()
+  run_workload_task = run_workload(build_recipe_command_task)
+  wait_for_workload_completion_task = wait_for_workload_completion()
+  clean_up_workload_task = clean_up_workload()
+
+  # Set the execution order.
+  (
+    build_recipe_command_task
+    >> run_workload_task
+    >> wait_for_workload_completion_task
+    >> clean_up_workload_task
+  )
