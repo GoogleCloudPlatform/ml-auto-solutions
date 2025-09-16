c38a21e4ab21bb55a5b5e13edb522a5842773d75
juliekuo <juliekuo@google.com>
2025-09-16 16:40:36 +0800

    Fix the format checking error, add time_out_in_min parameter

--- dags/maxtext_pathways/configs/commands.py
+++ dags/maxtext_pathways/configs/commands.py
@@ -1,55 +1,54 @@
 ENV_COMMAND = (
-  "set -xue && "
-  "pwd && "
-  "ls && "
-  "pip list && "
+  'set -xue && '
+  'pwd && '
+  'ls && '
+  'pip list && '
   # install make
-  "sudo apt-get update && "
-  "sudo apt-get install -y make && "
+  'sudo apt-get update && '
+  'sudo apt-get install -y make && '
   # install docker
-  "sudo apt-get install ca-certificates curl && "
-  "sudo install -m 0755 -d /etc/apt/keyrings && "
-  "sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && "
-  "sudo chmod a+r /etc/apt/keyrings/docker.asc && "
-  "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && "
-  "sudo apt-get update && "
-  "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin kubectl && " # install all necessary packages in a single command to avoid any potential locking conflicts
-  "sudo /usr/bin/dockerd & sleep 20 && "
+  'sudo apt-get install ca-certificates curl && '
+  'sudo install -m 0755 -d /etc/apt/keyrings && '
+  'sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc && '
+  'sudo chmod a+r /etc/apt/keyrings/docker.asc && '
+  "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo '$VERSION_CODENAME') stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && "
+  'sudo apt-get update && '
+  'sudo apt-get install -y kubectl docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && '  # install all necessary packages in a single command to avoid any potential locking conflicts
+  'sudo /usr/bin/dockerd & sleep 120 && '
+  'set -xue && ' # perform ' & ' will cancel it, need to set again
   # install gcloud
-  "curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz && "
-  "tar -xf google-cloud-cli-linux-x86_64.tar.gz && "
-  "./google-cloud-sdk/install.sh && "
-  "./google-cloud-sdk/bin/gcloud init && "
+  'curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz && '
+  'tar -xf google-cloud-cli-linux-x86_64.tar.gz && '
+  './google-cloud-sdk/install.sh && '
+  './google-cloud-sdk/bin/gcloud init && '
   # install kubectl
-  "grep -rhE ^deb /etc/apt/sources.list* | grep \"cloud-sdk\" && "
-  "kubectl version --client && "
+  "grep -rhE ^deb /etc/apt/sources.list* | grep 'cloud-sdk' && "
+  'kubectl version --client && '
   # install kubectl-kjob
-  "curl -Lo ./kubectl-kjob https://github.com/kubernetes-sigs/kjob/releases/download/v0.1.0/kubectl-kjob-linux-amd64 && "
-  "chmod +x ./kubectl-kjob && "
-  "sudo mv ./kubectl-kjob /usr/local/bin/kubectl-kjob && "
+  'curl -Lo ./kubectl-kjob https://github.com/kubernetes-sigs/kjob/releases/download/v0.1.0/kubectl-kjob-linux-amd64 && '
+  'chmod +x ./kubectl-kjob && '
+  'sudo mv ./kubectl-kjob /usr/local/bin/kubectl-kjob && '
   # install kubectl-kueue
-  "curl -Lo ./kubectl-kueue https://github.com/kubernetes-sigs/kueue/releases/download/v0.13.3/kubectl-kueue-linux-amd64 && "
-  "chmod +x ./kubectl-kueue && "
-  "sudo mv ./kubectl-kueue /usr/local/bin/kubectl-kueue && "
+  'curl -Lo ./kubectl-kueue https://github.com/kubernetes-sigs/kueue/releases/download/v0.13.3/kubectl-kueue-linux-amd64 && '
+  'chmod +x ./kubectl-kueue && '
+  'sudo mv ./kubectl-kueue /usr/local/bin/kubectl-kueue && '
   # install xpk
-  "git clone https://github.com/google/xpk.git ~/xpk && "
-  "cd ~/xpk && "
-  "python3 -m venv --system-site-packages .venv && "
-  "source .venv/bin/activate && "
-  "make install && "
-  "export PATH=$PATH:$HOME/xpk/bin && "
+  'git clone https://github.com/google/xpk.git ~/xpk && '
+  'cd ~/xpk && '
+  'python3 -m venv --system-site-packages .venv && '
+  'source .venv/bin/activate && '
+  'make install && '
+  'export PATH=$PATH:$HOME/xpk/bin && '
   # back to maxtext and give it a USER to `pip install --upgrade pip`
-  "cd /deps && "
-  "export USER=root",
+  'cd /deps && '
+  'export USER=root'
 )
 
-RECIPE_COMMAND = (
-  "python3 -m benchmarks.recipes.pw_mcjax_benchmark_recipe",
-)
+RECIPE_COMMAND = ('python3 -m benchmarks.recipes.pw_mcjax_benchmark_recipe')
 
 
 if __name__ == '__main__':
-  print(f"{ENV_COMMAND = }\n")
-  print(f"{RECIPE_COMMAND = }\n")
-  COMMAND = ENV_COMMAND[0] + " && " + RECIPE_COMMAND[0]
-  print(COMMAND.replace("&& ", "&& \n"))
+  print(f'{ENV_COMMAND = }\n')
+  print(f'{RECIPE_COMMAND = }\n')
+  TOTAL_COMMAND = ENV_COMMAND + ' && ' + RECIPE_COMMAND
+  print(f'{TOTAL_COMMAND = }\n')
--- dags/maxtext_pathways/configs/model_configs.py
+++ dags/maxtext_pathways/configs/model_configs.py
@@ -1,90 +1,98 @@
 # Copyright 2025 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Common model perf configs"""
+'''Common model perf configs'''
 
 import enum
 
 
 class MaxTextV5eModelConfigs(enum.Enum):
   # Refers to model configs in https://github.com/AI-Hypercomputer/maxtext/blob/main/benchmarks/maxtext_v5e_model_configs.py
-  DEFAULT_16B_V5E_256 = "default_16b_v5e_256"
-  DEFAULT_32B_V5E_256 = "default_32b_v5e_256"
-  DEFAULT_64B_V5E_256 = "default_64b_v5e_256"
-  DEFAULT_128B_V5E_256 = "default_128b_v5e_256"
-  GPT_3_175B_V5E_256 = "gpt_3_175b_v5e_256"
-  LLAMA2_7B_V5E_256 = "llama2_7b_v5e_256"
-  LLAMA2_13B_V5E_256 = "llama2_13b_v5e_256"
-  LLAMA2_70B_V5E_256 = "llama2_70b_v5e_256"
-  LLAMA3_1_8B_8192_V5E_256 = "llama3_1_8b_8192_v5e_256"
+  DEFAULT_16B_V5E_256 = 'default_16b_v5e_256'
+  DEFAULT_32B_V5E_256 = 'default_32b_v5e_256'
+  DEFAULT_64B_V5E_256 = 'default_64b_v5e_256'
+  DEFAULT_128B_V5E_256 = 'default_128b_v5e_256'
+  GPT_3_175B_V5E_256 = 'gpt_3_175b_v5e_256'
+  LLAMA2_7B_V5E_256 = 'llama2_7b_v5e_256'
+  LLAMA2_13B_V5E_256 = 'llama2_13b_v5e_256'
+  LLAMA2_70B_V5E_256 = 'llama2_70b_v5e_256'
+  LLAMA3_1_8B_8192_V5E_256 = 'llama3_1_8b_8192_v5e_256'
 
 
 class MaxTextV5pModelConfigs(enum.Enum):
   # Refers to model configs in https://github.com/AI-Hypercomputer/maxtext/blob/main/benchmarks/maxtext_v5p_model_configs.py
-  DEEPSEEK_V3_EP_256_V5P_512 = "deepseek_v3_ep_256_v5p_512"
-  LLAMA4_SCOUT_DROPLESS_V5P_256 = "llama4_scout_dropless_v5p_256"
-  LLAMA4_MAVERICK_DROPLESS_V5P_256 = "llama4_maverick_dropless_v5p_256"
-  LLAMA2_70B_V5P_128 = "llama2_70b_v5p_128"
-  LLAMA2_7B_V5P_128 = "llama2_7b_v5p_128"
-  GPT_3_175B_V5P_128 = "gpt_3_175b_v5p_128"
-  GPT_3_175B_V5P_128_SC = "gpt_3_175b_v5p_128_sc"
+  DEEPSEEK_V3_EP_256_V5P_512 = 'deepseek_v3_ep_256_v5p_512'
+  LLAMA4_SCOUT_DROPLESS_V5P_256 = 'llama4_scout_dropless_v5p_256'
+  LLAMA4_MAVERICK_DROPLESS_V5P_256 = 'llama4_maverick_dropless_v5p_256'
+  LLAMA2_70B_V5P_128 = 'llama2_70b_v5p_128'
+  LLAMA2_7B_V5P_128 = 'llama2_7b_v5p_128'
+  GPT_3_175B_V5P_128 = 'gpt_3_175b_v5p_128'
+  GPT_3_175B_V5P_128_SC = 'gpt_3_175b_v5p_128_sc'
 
 
 class MaxTextTrilliumModelConfigs(enum.Enum):
   # Refers to model configs in https://github.com/AI-Hypercomputer/maxtext/blob/main/benchmarks/maxtext_trillium_model_configs.py
-  DEFAULT_BASIC_1 = "default_basic_1"
-  DEFAULT_32 = "default_32"
-  DEFAULT_64 = "default_64"
-  DEFAULT_128 = "default_128"
-  DEFAULT_256 = "default_256"
-  DEFAULT_512 = "default_512"
-  GPT_3_175B = "gpt_3_175b"
-  GPT_3_175B_BF16 = "gpt_3_175b_bf16"
-  LLAMA2_7B_4096 = "llama2_7b_4096"
-  LLAMA2_70B_4096 = "llama2_70b_4096"
-  LLAMA2_70B_4096_SYNTHETIC = "llama2_70b_4096_synthetic"
-  LLAMA2_70B_4096_SC = "llama2_70b_4096_sc"
-  LLAMA2_70B_4096_SC_REAL_DATA_TFDS = "llama2_70b_4096_sc_real_data_tfds"
-  LLAMA2_70B_4096_SC_REAL_DATA_GRAIN = "llama2_70b_4096_sc_real_data_grain"
-  LLAMA2_70B_4096_SC_REAL_DATA_GRAIN_CHECKPOINT = "llama2_70b_4096_sc_real_data_grain_checkpoint"
-  LLAMA2_70B_4096_RD_LR = "llama2_70b_4096_rd_lr"
-  LLAMA3_8B_8192 = "llama3_8b_8192"
-  LLAMA3_70B_8192 = "llama3_70b_8192"
-  LLAMA3_1_405B_8192_FSDP_DCN = "llama3_1_405b_8192_fsdp_dcn"
-  LLAMA3_1_405B_8192_PURE_FSDP_ICI = "llama3_1_405b_8192_pure_fsdp_ici"
-  LLAMA3_1_8B_8192 = "llama3_1_8b_8192"
-  LLAMA3_1_8B_8192_BS5 = "llama3_1_8b_8192_bs5"
-  LLAMA3_1_8B_8192_NO_COLLECTIVE_MATMUL = "llama3_1_8b_8192_no_collective_matmul"
-  LLAMA3_1_70B_8192 = "llama3_1_70b_8192"
-  LLAMA3_1_70B_8192_BS2 = "llama3_1_70b_8192_bs2"
-  LLAMA3_1_70B_8192_BS2_BFLOAT16_NO_COLLECTIVE_MATMUL = "llama3_1_70b_8192_bs2_bfloat16_no_collective_matmul"
-  LLAMA3_1_70B_8192_BS4 = "llama3_1_70b_8192_bs4"
-  LLAMA3_1_70B_8192_SYNTHETIC = "llama3_1_70b_8192_synthetic"
-  LLAMA3_1_70B_8192_RD_GRAIN = "llama3_1_70b_8192_rd_grain"
-  LLAMA3_1_70B_8192_SYNTHETIC_CKPT = "llama3_1_70b_8192_synthetic_ckpt"
-  LLAMA3_1_70B_8192_RD_CKPT_GRAIN = "llama3_1_70b_8192_rd_ckpt_grain"
-  LLAMA3_1_70B_8192_PW_LR_RD = "llama3_1_70b_8192_pw_lr_rd"
-  LLAMA3_1_70B_8192_ITER_REAL_DATA_AND_CHECKPOINTING_TFDS = "llama3_1_70b_8192_iter_real_data_and_checkpointing_tfds"
-  LLAMA3_1_70B_8192_SYNTH = "llama3_1_70b_8192_synth"
-  LLAMA3_1_70B_129024 = "llama3_1_70b_129024"
-  MISTRAL_7B = "mistral_7b"
-  MIXTRAL_8X7B_DROPLESS = "mixtral_8x7b_dropless"
-  MIXTRAL_8X7B_DROPPED = "mixtral_8x7b_dropped"
-  MIXTRAL_8X7B_DROPPED_INT8 = "mixtral_8x7b_dropped_int8"
-  MIXTRAL_8X22B_DROPPED = "mixtral_8x22b_dropped"
-  DEEPSEEK_V3_EP16 = "deepseek_v3_ep16"
-  GEMMA2_9B_8192 = "gemma2_9b_8192"
-  GEMMA2_27B_8192 = "gemma2_27b_8192"
-  LLAMA3_1_70B_131072 = "llama3_1_70b_131072"
-  CUSTOM_MOE_700B = "custom_moe_700b"
+  DEFAULT_BASIC_1 = 'default_basic_1'
+  DEFAULT_32 = 'default_32'
+  DEFAULT_64 = 'default_64'
+  DEFAULT_128 = 'default_128'
+  DEFAULT_256 = 'default_256'
+  DEFAULT_512 = 'default_512'
+  GPT_3_175B = 'gpt_3_175b'
+  GPT_3_175B_BF16 = 'gpt_3_175b_bf16'
+  LLAMA2_7B_4096 = 'llama2_7b_4096'
+  LLAMA2_70B_4096 = 'llama2_70b_4096'
+  LLAMA2_70B_4096_SYNTHETIC = 'llama2_70b_4096_synthetic'
+  LLAMA2_70B_4096_SC = 'llama2_70b_4096_sc'
+  LLAMA2_70B_4096_SC_REAL_DATA_TFDS = 'llama2_70b_4096_sc_real_data_tfds'
+  LLAMA2_70B_4096_SC_REAL_DATA_GRAIN = 'llama2_70b_4096_sc_real_data_grain'
+  LLAMA2_70B_4096_SC_REAL_DATA_GRAIN_CHECKPOINT = (
+      'llama2_70b_4096_sc_real_data_grain_checkpoint'
+  )
+  LLAMA2_70B_4096_RD_LR = 'llama2_70b_4096_rd_lr'
+  LLAMA3_8B_8192 = 'llama3_8b_8192'
+  LLAMA3_70B_8192 = 'llama3_70b_8192'
+  LLAMA3_1_405B_8192_FSDP_DCN = 'llama3_1_405b_8192_fsdp_dcn'
+  LLAMA3_1_405B_8192_PURE_FSDP_ICI = 'llama3_1_405b_8192_pure_fsdp_ici'
+  LLAMA3_1_8B_8192 = 'llama3_1_8b_8192'
+  LLAMA3_1_8B_8192_BS5 = 'llama3_1_8b_8192_bs5'
+  LLAMA3_1_8B_8192_NO_COLLECTIVE_MATMUL = (
+      'llama3_1_8b_8192_no_collective_matmul'
+  )
+  LLAMA3_1_70B_8192 = 'llama3_1_70b_8192'
+  LLAMA3_1_70B_8192_BS2 = 'llama3_1_70b_8192_bs2'
+  LLAMA3_1_70B_8192_BS2_BFLOAT16_NO_COLLECTIVE_MATMUL = (
+      'llama3_1_70b_8192_bs2_bfloat16_no_collective_matmul'
+  )
+  LLAMA3_1_70B_8192_BS4 = 'llama3_1_70b_8192_bs4'
+  LLAMA3_1_70B_8192_SYNTHETIC = 'llama3_1_70b_8192_synthetic'
+  LLAMA3_1_70B_8192_RD_GRAIN = 'llama3_1_70b_8192_rd_grain'
+  LLAMA3_1_70B_8192_SYNTHETIC_CKPT = 'llama3_1_70b_8192_synthetic_ckpt'
+  LLAMA3_1_70B_8192_RD_CKPT_GRAIN = 'llama3_1_70b_8192_rd_ckpt_grain'
+  LLAMA3_1_70B_8192_PW_LR_RD = 'llama3_1_70b_8192_pw_lr_rd'
+  LLAMA3_1_70B_8192_ITER_REAL_DATA_AND_CHECKPOINTING_TFDS = (
+      'llama3_1_70b_8192_iter_real_data_and_checkpointing_tfds'
+  )
+  LLAMA3_1_70B_8192_SYNTH = 'llama3_1_70b_8192_synth'
+  LLAMA3_1_70B_129024 = 'llama3_1_70b_129024'
+  MISTRAL_7B = 'mistral_7b'
+  MIXTRAL_8X7B_DROPLESS = 'mixtral_8x7b_dropless'
+  MIXTRAL_8X7B_DROPPED = 'mixtral_8x7b_dropped'
+  MIXTRAL_8X7B_DROPPED_INT8 = 'mixtral_8x7b_dropped_int8'
+  MIXTRAL_8X22B_DROPPED = 'mixtral_8x22b_dropped'
+  DEEPSEEK_V3_EP16 = 'deepseek_v3_ep16'
+  GEMMA2_9B_8192 = 'gemma2_9b_8192'
+  GEMMA2_27B_8192 = 'gemma2_27b_8192'
+  LLAMA3_1_70B_131072 = 'llama3_1_70b_131072'
+  CUSTOM_MOE_700B = 'custom_moe_700b'
