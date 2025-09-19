931f86de8c29817fea16b7496ba8eb30dbb8dca6
juliekuo <juliekuo@google.com>
2025-09-19 12:01:53 +0800

    Adjust the statement and add more document markdown
    1. Use join rather than manually adding " && " for each statement in commands.py
    2. Modify the doc_md in pw_mcjax_benchmark_recipe_dag.py

--- dags/maxtext_pathways/pw_mcjax_benchmark_recipe_dag.py
+++ dags/maxtext_pathways/pw_mcjax_benchmark_recipe_dag.py
@@ -26,10 +26,11 @@ with DAG(
 
   # Description
   Specify different models and number of slices to test the MaxText pw_mcjax_benchmark_recipe on different clusters.  
-  The DAG first receives the function package through the UI parameters, then runs the workload, waits and monitors the workload logs, and finally cleans up the workload.
+  The DAG first generates recipe command through UI parameters, then runs the workload, waits and monitors the workload logs, and finally cleans up the workload.
 
   ### Prerequisites
-  This test requires an existing cluster.
+  This test requires an existing cluster.  
+  This test requires that a bucket with the same name as the UI parameter "[User]-[Region]" exists in the UI parameter [Project].
 
   ### Procedures
   An Airflow Composer environment must be created, and the required DAG code must be deployed to the associated GCS bucket.  
@@ -40,7 +41,7 @@ with DAG(
   build_recipe_command_task = build_recipe_command()
   run_workload_task = run_workload(build_recipe_command_task)
   wait_for_workload_completion_task = wait_for_workload_completion()
-  clean_up_workload_task = clean_up_workload().as_teardown(setups=run_workload_task) # TODO: check the logic of setups
+  clean_up_workload_task = clean_up_workload().as_teardown(setups=run_workload_task)
 
   # Set the execution order.
   (
