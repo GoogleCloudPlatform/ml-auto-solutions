37ec46e3ebd7f5939b86cfd00c0b84bcfd65286b
CJ Tsai <cj.tsai@cienet.com>
2025-10-08 14:31:03 +0800

    Add custom timeout feature for recipe completion monitoring

--- dags/maxtext_pathways/pw_mcjax_benchmark_recipe.py
+++ dags/maxtext_pathways/pw_mcjax_benchmark_recipe.py
@@ -2,13 +2,12 @@ import datetime
 from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
-from airflow.providers.cncf.kubernetes.utils.pod_manager import OnFinishAction
 from kubernetes.client import models as k8s
 
 from dags.common.vm_resource import DockerImage
 from dags.maxtext_pathways.configs.parameters import PARAMETERS
 from dags.maxtext_pathways.configs.commands import COMMAND_DELETE_POD_STR
-from dags.maxtext_pathways.utils.tasks import get_parameters
+from dags.maxtext_pathways.utils.tasks import get_parameters, set_sensor_timeout
 from xlml.utils.xpk import wait_for_workload_completion, clean_up_workload
 
 
@@ -34,71 +33,77 @@ with DAG(
   # A DAG to run a MaxText {RECIPE} on GKE.
 
   ### Description
-  Specify different models and number of slices to test the MaxText {RECIPE} on different clusters.  
-  The DAG first generates recipe command through UI parameters, then runs the workload, waits and monitors the workload logs, and finally cleans up the workload.  
+  Specify different models and number of slices to test the MaxText {RECIPE} on different clusters.
+  The DAG first generates recipe command through UI parameters, then runs the workload, waits and monitors the workload logs, and finally cleans up the workload.
 
   ### Prerequisites
-  - This test requires an existing cluster.  
-  - Create a service account with the following roles: `Artifact Registry Reader`, `Kubernetes Engine Admin`, `Monitoring Viewer`.   
+  - This test requires an existing cluster.
+  - Create a service account with the following roles: `Artifact Registry Reader`, `Kubernetes Engine Admin`, `Monitoring Viewer`.
     - Generate a new service account key and download the JSON file to retrieve its contents. Next, create a secret manager named `one-click-key` and store the key contents there for use when switching service accounts.
     - Make sure the default service account has the `Secret Manager Secret Accessor` role.
-  - If you're using a service account to pull an image from a different project, you need to grant the service account the `Artifact Registry Reader` role in that project.  
+  - If you're using a service account to pull an image from a different project, you need to grant the service account the `Artifact Registry Reader` role in that project.
 
   ### Procedures
-  An Airflow Composer environment must be created, and the required DAG code must be deployed to the associated GCS bucket.  
-  To initiate the recipe, the user must access the Airflow UI, locate the specific DAG, and trigger its execution.  
+  An Airflow Composer environment must be created, and the required DAG code must be deployed to the associated GCS bucket.
+  To initiate the recipe, the user must access the Airflow UI, locate the specific DAG, and trigger its execution.
   """,
 ) as dag:
-    # Define task dependencies by instantiating and linking tasks.
-    params = get_parameters()
+  # Define task dependencies by instantiating and linking tasks.
+  params = get_parameters()
 
-    recipe_runtime = (
-        RECIPE.replace("_", "-") + '-{{ execution_date.strftime("%H%M%S") }}'
-    )
+  recipe_runtime = (
+      RECIPE.replace("_", "-") + '-{{ execution_date.strftime("%H%M%S") }}'
+  )
 
-    start_recipe = GKEStartPodOperator(
-        task_id="start_recipe",
-        name=RECIPE.replace("_", "-"),
-        project_id=params["project"],
-        cluster_name=params["cluster_name"],
-        location=params["region"],
-        namespace="default",
-        hostnetwork=True,
-        image=DockerImage.MAXTEXT_TPU_JAX_NIGHTLY.value,
-        # on_finish_action=OnFinishAction.DELETE_POD.value,  # TODO: Unable to delete the pod, may need to update the Airflow version.
-        get_logs=True,
-        cmds=["/bin/bash", "-cxue", params["commands"]],
-        container_security_context=k8s.V1SecurityContext(privileged=True),
-        labels={"airflow-runtime": recipe_runtime},
-    )
+  start_recipe = GKEStartPodOperator(
+      task_id="start_recipe",
+      name=RECIPE.replace("_", "-"),
+      project_id=params["project"],
+      cluster_name=params["cluster_name"],
+      location=params["region"],
+      namespace="default",
+      hostnetwork=True,
+      image=DockerImage.MAXTEXT_TPU_JAX_NIGHTLY.value,
+      # on_finish_action=OnFinishAction.DELETE_POD.value,  # TODO: Unable to delete the pod, may need to update the Airflow version.
+      get_logs=True,
+      cmds=["/bin/bash", "-cxue", params["commands"]],
+      container_security_context=k8s.V1SecurityContext(privileged=True),
+      labels={"airflow-runtime": recipe_runtime},
+  )
 
-    clean_up_pod = BashOperator(
-        task_id="clean_up_pod",
-        bash_command=(
-            COMMAND_DELETE_POD_STR.format(
-                cluster_name=params["cluster_name"],
-                region=params["region"],
-                project=params["project"],
-                airflow_runtime=recipe_runtime,
-            )
-        ),
-    )
+  clean_up_pod = BashOperator(
+      task_id="clean_up_pod",
+      bash_command=(
+          COMMAND_DELETE_POD_STR.format(
+              cluster_name=params["cluster_name"],
+              region=params["region"],
+              project=params["project"],
+              airflow_runtime=recipe_runtime,
+          )
+      ),
+  )
 
-    check_recipe_log = wait_for_workload_completion.override(
-        task_id="check_recipe_log"
-    )(
-        workload_id=params["recipe_workload_id"],
-        project_id=params["project"],
-        region=params["region"],
-        cluster_name=params["cluster_name"],
-    )
+  check_recipe_log = wait_for_workload_completion.override(
+      task_id="check_recipe_log", on_execute_callback=[set_sensor_timeout]
+  )(
+      workload_id=params["recipe_workload_id"],
+      project_id=params["project"],
+      region=params["region"],
+      cluster_name=params["cluster_name"],
+  )
 
-    clean_up_recipe = clean_up_workload.override(task_id="clean_up_recipe")(
-        workload_id=params["recipe_workload_id"],
-        project_id=params["project"],
-        zone=params["zone"],
-        cluster_name=params["cluster_name"],
-    ).as_teardown(setups=start_recipe)
+  clean_up_recipe = clean_up_workload.override(task_id="clean_up_recipe")(
+      workload_id=params["recipe_workload_id"],
+      project_id=params["project"],
+      zone=params["zone"],
+      cluster_name=params["cluster_name"],
+  ).as_teardown(setups=start_recipe)
 
-    # Set the execution order.
-    (params >> start_recipe >> clean_up_pod >> check_recipe_log >> clean_up_recipe)
+  # Set the execution order.
+  (
+      params
+      >> start_recipe
+      >> clean_up_pod
+      >> check_recipe_log
+      >> clean_up_recipe
+  )
--- dags/maxtext_pathways/utils/tasks.py
+++ dags/maxtext_pathways/utils/tasks.py
@@ -4,83 +4,80 @@ import string
 import time
 from absl import logging
 from airflow.decorators import task
-from airflow.operators.python import get_current_context
 
 from dags.maxtext_pathways.configs.commands import COMMAND_ENV, COMMAND_RECIPE
 from xlml.utils.gke import zone_to_region
 
 
 @task.python(multiple_outputs=True)
-def get_parameters():
-    """Generate parameteres form DAG UI input."""
-    # Get the DAG run"s configuration and parameters.
-    context = get_current_context()
-    params = context.get("params", {})
+def get_parameters(**context):
+  """Generate parameteres form DAG UI input."""
+  # Get the DAG run"s configuration and parameters.
+  params = context.get("params", {})
 
-    # Initialization command.
-    recipe_cmds = COMMAND_RECIPE
+  # Initialization command.
+  recipe_cmds = COMMAND_RECIPE
 
-    # Generate recipe workload_id and temp_key.
-    name, temp_post_fix = generate_recipe_workload_id(params)
-    params["temp_key"] = temp_post_fix
-    params["recipe_workload_id"] = name
+  # Generate recipe workload_id and temp_key.
+  name, temp_post_fix = generate_recipe_workload_id(params)
+  params["temp_key"] = temp_post_fix
+  params["recipe_workload_id"] = name
 
-    # Combine command.
-    device_type = params["device_version"] + "-" + str(params["core_count"])
-    params["device_type"] = device_type
+  # Combine command.
+  device_type = params["device_version"] + "-" + str(params["core_count"])
+  params["device_type"] = device_type
 
-    for key, value in params.items():
-        if key not in [
-            "time_out_in_min",
-            "device_version",
-            "core_count",
-            "service_account",
-            "recipe_workload_id",
-        ]:
-            if isinstance(value, int):
-                recipe_cmds += f" --{key}={value}"
-            else:
-                recipe_cmds += f" --{key}='{value}'"
+  for key, value in params.items():
+    if key not in [
+        "time_out_in_min",
+        "device_version",
+        "core_count",
+        "service_account",
+        "recipe_workload_id",
+    ]:
+      if isinstance(value, int):
+        recipe_cmds += f" --{key}={value}"
+      else:
+        recipe_cmds += f" --{key}='{value}'"
 
-    formatted_cmds = recipe_cmds.replace(" --", " \n  --")
-    logging.info(f"\n {formatted_cmds}")
+  formatted_cmds = recipe_cmds.replace(" --", " \n  --")
+  logging.info(f"\n {formatted_cmds}")
 
-    env_cmds = COMMAND_ENV.format(service_account=params["service_account"])
+  env_cmds = COMMAND_ENV.format(service_account=params["service_account"])
 
-    # Add parameters.
-    params["commands"] = " && ".join([env_cmds, recipe_cmds])
-    params["region"] = zone_to_region(params["zone"])
-    params["time_out_in_sec"] = datetime.timedelta(
-        minutes=params["time_out_in_min"]
-    ).total_seconds()  # TODO: add it to wait_for_workload_completion
+  # Add parameters.
+  params["commands"] = " && ".join([env_cmds, recipe_cmds])
+  params["region"] = zone_to_region(params["zone"])
 
-    return params
+  return params
 
 
 def generate_recipe_workload_id(params):
-    """Please refer to the `generate_xpk_workload_cmd` function in the `/maxtext/benchmarks/maxtext_xpk_runner.py` file."""
-    time.localtime()
-    length_of_random_str = 3
-    temp_post_fix = "".join(
-        random.choice(string.ascii_lowercase + string.digits)
-        for _ in range(length_of_random_str)
-    )
+  """Please refer to the `generate_xpk_workload_cmd` function in the `/maxtext/benchmarks/maxtext_xpk_runner.py` file."""
+  time.localtime()
+  length_of_random_str = 3
+  temp_post_fix = "".join(
+      random.choice(string.ascii_lowercase + string.digits)
+      for _ in range(length_of_random_str)
+  )
 
-    truncate_model_name = 10
-    truncate_prefix = 3
-    post_fix = f'-{params["num_slices_list"]}-{time.strftime("%m%d%H", time.localtime())}-{temp_post_fix}'
-    common_prefix = params["user"]
+  truncate_model_name = 10
+  truncate_prefix = 3
+  post_fix = f'-{params["num_slices_list"]}-{time.strftime("%m%d%H", time.localtime())}-{temp_post_fix}'
+  common_prefix = params["user"]
 
-    pw_prefix = "pw-"
+  pw_prefix = "pw-"
 
-    if params["selected_model_framework"] == "pathways":
-        post_fix = f'-{params["num_slices_list"]}-{temp_post_fix}'
-        name = f'{pw_prefix}{params["selected_model_names"].replace("_", "-")[:truncate_model_name - len(pw_prefix)]}'
-    else:
-        name = (
-            f'{params["selected_model_names"].replace("_", "-")[:truncate_model_name]}'
-        )
+  if params["selected_model_framework"] == "pathways":
+    post_fix = f'-{params["num_slices_list"]}-{temp_post_fix}'
+    name = f'{pw_prefix}{params["selected_model_names"].replace("_", "-")[:truncate_model_name - len(pw_prefix)]}'
+  else:
+    name = f'{params["selected_model_names"].replace("_", "-")[:truncate_model_name]}'
 
-    name = f"{common_prefix[:truncate_prefix]}-{name}{post_fix}"
+  name = f"{common_prefix[:truncate_prefix]}-{name}{post_fix}"
 
-    return name, temp_post_fix
+  return name, temp_post_fix
+
+
+def set_sensor_timeout(context):
+  context["ti"].task.timeout = context["params"]["time_out_in_min"] * 60
