9d8b918671385f0bcd6260bda772ec8ecce8aae1
Jacky Fang <123862185+RexBearIU@users.noreply.github.com>
2025-10-13 12:21:58 +0800

    fix: modified the conflict schedule time (#76)

--- dags/orbax/maxtext_reg_save_gcs.py
+++ dags/orbax/maxtext_reg_save_gcs.py
@@ -20,7 +20,7 @@ from dags.orbax.util import validation_util, checkpoint_util, test_config_util
 from xlml.utils.xpk import MAIN_BRANCH
 from xlml.utils.gke import zone_to_region
 
-SCHEDULE = "0 16 * * *" if composer_env.is_prod_env() else None
+SCHEDULE = "0 12 * * *" if composer_env.is_prod_env() else None
 DAG_TEST_NAME = "maxtext_regular_save"
 
 # Only one version of the Docker image is supported at the moment.
