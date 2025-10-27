40cf3b51e4feddc18e9110c4c4b31f0fcb824656
severusho <severus.ho@cienet.com>
2025-10-27 17:52:59 +0800

    split nightly

--- dags/pytorch_xla/nightly-tpu.py
+++ dags/pytorch_xla/nightly-tpu.py
@@ -123,24 +123,6 @@ def torchvision():
   mnist_v2_8 >> (resnet_v2_8, *resnet_v4_8_tests, resnet_v4_32, resnet_v5lp_4)
   resnet_v2_8 >> resnet_v3_8_tests
 
-  resnet_v100_2x2 = task.GpuGkeTask(
-      test_config.GpuGkeTest.from_pytorch(
-          "pt-nightly-resnet50-mp-fake-v100-x2x2"
-      ),
-      US_CENTRAL1,
-      "gpu-uc1",
-  ).run()
-
-  resnet_v100_2x2_spmd = task.GpuGkeTask(
-      test_config.GpuGkeTest.from_pytorch(
-          "pt-nightly-resnet50-spmd-batch-fake-v100-x2x2"
-      ),
-      US_CENTRAL1,
-      "gpu-uc1",
-  ).run()
-
-  resnet_v100_2x2 >> resnet_v100_2x2_spmd
-
 
 @task_group(prefix_group_id=False)
 def huggingface():
