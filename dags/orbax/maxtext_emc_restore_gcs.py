006bd2df0c867a97533722a2af78ddbe856034a4
Depp Lee <ooops678@gmail.com>
2025-11-20 10:46:12 +0800

    Fix incorrect "pod_pattern" for log validation
    Fix incorrect pod_pattern for log validation
    
    ---------
    
    Co-authored-by: Depp Lee <depp.lee@cienet.com>

--- dags/orbax/maxtext_emc_restore_gcs.py
+++ dags/orbax/maxtext_emc_restore_gcs.py
@@ -141,7 +141,7 @@ with models.DAG(
                 project_id=test_config.cluster.project,
                 location=zone_to_region(test_config.cluster.zone),
                 cluster_name=test_config.cluster.name,
-                pod_pattern=f"{test_config.short_id}-emc.*1-0",
+                pod_pattern=f"{test_config.short_id}-emc.*0-0",
                 interrupt_at_step=step_to_interrupt,
                 start_time=start_time,
                 end_time=end_time,
