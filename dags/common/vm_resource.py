ac9c60b570935711aa1acea93aee20cb35bba14a
Camilo Quinones <camilo.quinones@cienet.com>
2026-01-06 13:23:50 +0000

    Fix: Fixed maxtext_muti_tier_checkpointing. Point to new cluster auto-v5p-8-bodaborg

--- dags/common/vm_resource.py
+++ dags/common/vm_resource.py
@@ -255,6 +255,13 @@ class XpkClusters:
       project=Project.CLOUD_TPU_MULTIPOD_DEV.value,
       zone=Zone.EUROPE_WEST4_B.value,
   )
+  TPU_V5P_8_CLUSTER_V2 = XpkClusterConfig(
+      name="auto-v5p-8-bodaborg",
+      device_version=TpuVersion.V5P,
+      core_count=8,
+      project=Project.CLOUD_TPU_MULTIPOD_DEV.value,
+      zone=Zone.EUROPE_WEST4_B.value,
+  )
   TPU_V5P_128_CLUSTER = XpkClusterConfig(
       name="v5p-128-bodaborg-europe-west4-b",
       device_version=TpuVersion.V5P,
