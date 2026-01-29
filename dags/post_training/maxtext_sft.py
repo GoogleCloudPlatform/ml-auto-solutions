d0d00f7f0e263b0be0d61e3e66ff32b01cd02fd2
Ran Ran <ran.rissy@gmail.com>
2026-01-29 11:27:40 -0800

    Revert "fix: Support multi-slice training and add experiment name (#1161)" (#1165)
    This reverts commit 5ee6a238b486995b7ba6da2c4e8b531e337e8c74.

--- dags/post_training/maxtext_sft.py
+++ dags/post_training/maxtext_sft.py
@@ -49,8 +49,6 @@ def run_training(
         test_owner=test_owner.JACKY_F,
     ).run_model(
         use_pathways=True,
-        use_vertex_tensorboard=True,
-        experiment_name="sft",
         xpk_branch=MAIN_BRANCH,
     )
 
@@ -130,7 +128,7 @@ with models.DAG(
   training_config = test_config_util.SFTTestConfig(
       cluster=XpkClusters.TPU_V5P_128_CLUSTER,
       accelerator="v5p-128",
-      slices=[2],
+      slices=[1],  # Single slice for SFT training
       model_name="llama3.1-70b",
       steps=training_steps,
       short_id="msft",
