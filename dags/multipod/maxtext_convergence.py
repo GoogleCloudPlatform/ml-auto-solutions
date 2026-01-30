e5a0b4c1595bfc676aeed2088a90a35e78532447
RUEI4341 <82101769+RUEI4341@users.noreply.github.com>
2026-01-30 16:30:45 +0800

    fix: Bigger eval_interval to avoid timeout (#1167)
    The training jobs of the DAG are timing out recently. Upon reviewing the logs from our jax-tpu container, we have identified a main performance issues:
    
    Periodic Slowdowns: We observe significant, recurring slowdowns in the training speed approximately every 100 training steps (e.g., around steps 100, 200, etc., up to the total of 10199 steps). While some slowdown at these intervals is also present in successful runs, the duration of these delays is much longer in the runs that eventually time out.
    
    Impact: The exacerbated periodic slowdowns is causing our training jobs to exceed their allocated time and fail with timeouts. The logs indicate that runs are timing out, whereas previously, they would complete.
    
    This PR increased the eval period (`eval_interval`) to 5000 to prevent periodic slowdowns in the training.

--- dags/multipod/maxtext_convergence.py
+++ dags/multipod/maxtext_convergence.py
@@ -51,11 +51,13 @@ with models.DAG(
   steps = 10200  # Half Chinchilla
   loss_threshold = 2.7
   per_device_batch_size = 2.0  # 256 chips * 2 pdb = 512 gbs.
+  eval_interval = 5000
 
   base_convergence_command = (
       "bash tests/end_to_end/tpu/test_convergence_1b_params.sh"
       f" OUTPUT_PATH={base_output_directory} DATASET_PATH={dataset_path}"
       f" LOSS_THRESHOLD={loss_threshold} STEPS={steps}"
+      f" EVAL_INTERVAL={eval_interval}"
       f" PER_DEVICE_BATCH_SIZE={per_device_batch_size}"
   )
   convergence_tests = {
